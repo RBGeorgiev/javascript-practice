a31d9c2a3db8930fd2a8435468a7951e16559745
Rado <43698977+BaiRado@users.noreply.github.com>
2019-05-22 12:41:09 +0100

    brick collision makes it disappear

--- brick-breaker/src/brick.js
+++ brick-breaker/src/brick.js
@@ -1,5 +1,5 @@
 export default class Brick {
-    constructor(game) {
+    constructor(game, position) {
         this.gameWidth = game.gameWidth;
         this.gameHeight = game.gameHeight;
 
@@ -8,61 +8,70 @@ export default class Brick {
         this.width = 70;
         this.height = 30;
 
-        this.position = {
-            x: 280,
-            y: 100
-        }
+        this.position = position;
+
+        this.hit = false;
     }
 
 
     draw(ctx) {
-        ctx.fillStyle = "red";
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        if (!this.hit) {
+            ctx.fillStyle = "red";
+            ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+            ctx.strokeRect(this.position.x, this.position.y, this.width, this.height);
+        }
     }
 
-    update(deltaTime) {
-        // top side collision
-        if (
-            this.game.ball.position.y > this.position.y - this.game.ball.radius
-            && this.game.ball.position.y < this.position.y
-            && this.game.ball.position.x > this.position.x
-            && this.game.ball.position.x < this.position.x + this.width
-        ) {
-            this.game.ball.speed.y = -this.game.ball.speed.y;
-            this.game.ball.position.y = this.position.y - this.game.ball.radius;
-        }
+    update() {
+        if (!this.hit) {
 
-        // bottom side collision
-        if (
-            this.game.ball.position.y < this.position.y + this.height + this.game.ball.radius
-            && this.game.ball.position.y > this.position.y
-            && this.game.ball.position.x > this.position.x
-            && this.game.ball.position.x < this.position.x + this.width
-        ) {
-            this.game.ball.speed.y = -this.game.ball.speed.y;
-            this.game.ball.position.y = this.position.y + this.game.ball.radius + this.height
-        }
+            // top side collision
+            if (
+                this.game.ball.position.y > this.position.y - this.game.ball.radius
+                && this.game.ball.position.y < this.position.y
+                && this.game.ball.position.x > this.position.x
+                && this.game.ball.position.x < this.position.x + this.width
+            ) {
+                this.game.ball.speed.y = -this.game.ball.speed.y;
+                this.game.ball.position.y = this.position.y - this.game.ball.radius;
+                this.hit = true;
+            }
 
-        // left side collision
-        if (
-            this.game.ball.position.x > this.position.x - this.game.ball.radius
-            && this.game.ball.position.x < this.position.x
-            && this.game.ball.position.y > this.position.y
-            && this.game.ball.position.y < this.position.y + this.height
-        ) {
-            this.game.ball.speed.x = -this.game.ball.speed.x;
-            this.game.ball.position.x = this.position.x - this.game.ball.radius
-        }
+            // bottom side collision
+            if (
+                this.game.ball.position.y < this.position.y + this.height + this.game.ball.radius
+                && this.game.ball.position.y > this.position.y
+                && this.game.ball.position.x > this.position.x
+                && this.game.ball.position.x < this.position.x + this.width
+            ) {
+                this.game.ball.speed.y = -this.game.ball.speed.y;
+                this.game.ball.position.y = this.position.y + this.game.ball.radius + this.height;
+                this.hit = true;
+            }
 
-        //right side collision
-        if (
-            this.game.ball.position.x < this.position.x + this.width + this.game.ball.radius
-            && this.game.ball.position.x > this.position.x
-            && this.game.ball.position.y > this.position.y
-            && this.game.ball.position.y < this.position.y + this.height
-        ) {
-            this.game.ball.speed.x = -this.game.ball.speed.x;
-            this.game.ball.position.x = this.position.x + this.width + this.game.ball.radius;
+            // left side collision
+            if (
+                this.game.ball.position.x > this.position.x - this.game.ball.radius
+                && this.game.ball.position.x < this.position.x
+                && this.game.ball.position.y > this.position.y
+                && this.game.ball.position.y < this.position.y + this.height
+            ) {
+                this.game.ball.speed.x = -this.game.ball.speed.x;
+                this.game.ball.position.x = this.position.x - this.game.ball.radius;
+                this.hit = true;
+            }
+
+            //right side collision
+            if (
+                this.game.ball.position.x < this.position.x + this.width + this.game.ball.radius
+                && this.game.ball.position.x > this.position.x
+                && this.game.ball.position.y > this.position.y
+                && this.game.ball.position.y < this.position.y + this.height
+            ) {
+                this.game.ball.speed.x = -this.game.ball.speed.x;
+                this.game.ball.position.x = this.position.x + this.width + this.game.ball.radius;
+                this.hit = true;
+            }
         }
     }
 }
--- brick-breaker/src/game.js
+++ brick-breaker/src/game.js
@@ -12,19 +12,31 @@ export default class Game {
     start() {
         this.paddle = new Paddle(this);
         this.ball = new Ball(this);
-        this.brick = new Brick(this);
+
+        // placeholder/test bricks
+        this.bricks = [
+            new Brick(this, { x: 280, y: 100 }),
+            new Brick(this, { x: 280 + 70, y: 100 }),
+            new Brick(this, { x: 280 + 70 * 2, y: 100 }),
+            new Brick(this, { x: 280 + 70 * 3, y: 100 }),
+            new Brick(this, { x: 280 + 70 * 4, y: 100 }),
+            new Brick(this, { x: 280 + 70 * 5, y: 100 }),
+            new Brick(this, { x: 280 + 70 * 6, y: 100 }),
+            new Brick(this, { x: 280 + 70 * 7, y: 100 }),
+        ];
+
         new InputHandler(this.paddle);
     }
 
     draw(ctx) {
         this.paddle.draw(ctx);
         this.ball.draw(ctx);
-        this.brick.draw(ctx)
+        this.bricks.forEach(brick => brick.draw(ctx));
     }
 
     update(deltaTime) {
         this.paddle.update(deltaTime);
         this.ball.update(deltaTime);
-        this.brick.update(deltaTime);
+        this.bricks.forEach(brick => brick.update());
     }
 }
