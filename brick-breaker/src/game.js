6c003e998915a313fc46dc6fba3e22e83ebb62ff
Rado <43698977+BaiRado@users.noreply.github.com>
2019-05-23 17:18:04 +0100

    added win condition

--- brick-breaker/src/game.js
+++ brick-breaker/src/game.js
@@ -25,21 +25,34 @@ export default class Game {
         this.paused = !this.paused;
     }
 
-    draw(ctx) {
-        this.paddle.draw(ctx);
-        this.ball.draw(ctx);
-        this.bricks.forEach(brick => brick.draw(ctx));
-
-        if (this.paused) {
-            ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-            ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
-        }
-    }
-
     update(deltaTime) {
         if (this.paused) return
         this.paddle.update(deltaTime);
         this.ball.update(deltaTime);
+
+        this.bricks = this.bricks.filter(brick => !brick.hit);
         this.bricks.forEach(brick => brick.update());
+
+        console.log(this.bricks)
+    }
+
+    draw(ctx) {
+        this.paddle.draw(ctx);
+        this.ball.draw(ctx);
+
+        this.bricks.forEach(brick => brick.draw(ctx));
+
+        // paused overlay placeholder
+        if (this.paused) {
+            ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
+            ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+        }
+
+        // won overlay placeholder
+        if (this.bricks.length === 0) {
+            this.paused = true;
+            ctx.fillStyle = "rgba(255, 255, 0, 0.6)";
+            ctx.fillRect(0, 0, this.gameWidth, this.gameHeight);
+        }
     }
 }
