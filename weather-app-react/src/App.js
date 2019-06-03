c9e151db4578de11189f7e2da4482c25b992fb14
Rado <43698977+BaiRado@users.noreply.github.com>
2019-06-03 23:50:14 +0100

    added state

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,15 +1,44 @@
 import React from "react";
 
+import { API_KEY } from './components/Constants';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
 class App extends React.Component {
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined,
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const CITY = e.target.city.value;
+    const COUNTRY = e.target.country.value;
+    const API_CALL = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY},${COUNTRY}&units=metric&appid=${API_KEY}`);
+    const DATA = await API_CALL.json();
+
+    console.log(DATA);
+
+    this.setState({
+      city: DATA.name,
+      country: DATA.sys.country,
+      temperature: DATA.main.temp,
+      humidity: DATA.main.humidity,
+      description: DATA.weather[0].description,
+      error: undefined,
+    })
+  }
+
   render() {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     );
