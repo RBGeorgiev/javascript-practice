ee2ca8df85cdf71842cebc1c335d973c1d581931
Rado <43698977+BaiRado@users.noreply.github.com>
2019-06-10 00:04:43 +0100

    styled contact

--- landing-page/src/components/Form.js
+++ landing-page/src/components/Form.js
@@ -2,22 +2,23 @@ import React from "react";
 
 const Form = () => (
     <form>
-        <label>Name:<input type="text" name="name" required></input></label>
-        <label>Email:<input type="text" name="email" required></input></label>
-
+        <div id="contact-info">
+            <label>Name <input type="text" name="name" required></input></label><br></br>
+            <label>Email <input type="email" name="email" required></input></label>
+        </div >
         <div id="dates">
-            <label for="start">Start date:</label>
-            <input type="date" id="start" name="start-date"
+            <label for="start">Start date <input type="date" id="start" name="start-date"
                 value="2019-07-22"
-                min="2019-07-01" max="2019-12-31"></input>
+                min="2019-07-01" max="2019-12-31" required></input>
+            </label>
 
-            <label for="end">End date:</label>
-            <input type="date" id="end" name="end-date"
+            <label for="end">End date <input type="date" id="end" name="end-date"
                 value="2019-07-26"
-                min="2019-07-01" max="2019-12-31"></input>
+                min="2019-07-01" max="2019-12-31" required></input>
+            </label>
         </div>
-
-        <label>Message:<textarea type="text" name="message"></textarea></label>
+        <label for="message">Message </label>
+        <textarea id="message" name="message"></textarea>
         <button>Send</button>
     </form>
 );
