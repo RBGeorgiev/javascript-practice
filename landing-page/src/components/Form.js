231040f86b23fd0490e3a33309c5fb7cd047371d
Rado <43698977+BaiRado@users.noreply.github.com>
2019-06-11 23:17:15 +0100

    removed default on submit

--- landing-page/src/components/Form.js
+++ landing-page/src/components/Form.js
@@ -1,27 +1,37 @@
 import React from "react";
 
-const Form = () => (
-    <form>
-        <div id="contact-info">
-            <label>Name <input type="text" name="name" required></input></label>
-            <label class="style-right">Email <input type="email" name="email" required></input></label>
-        </div >
-        <div id="dates">
-            <label for="start">Start date </label>
-            <input type="date" id="start" name="start-date"
-                value="2019-07-22"
-                min="2019-07-01" max="2019-12-31" required></input>
+class Form extends React.Component {
+    onSubmit(e) {
+        e.preventDefault();
+    }
 
-            <label class="style-right" for="end">End date </label>
-            <input type="date" id="end" name="end-date"
-                value="2019-07-26"
-                min="2019-07-01" max="2019-12-31" required></input>
+    render() {
+        return (
+            <form onSubmit={this.onSubmit}>
+                <div id="contact-info">
+                    <label>Name <input type="text" name="name" required></input></label>
+                    <label class="style-right">Email <input type="email" name="email" required></input></label>
+                </div >
 
-        </div>
-        <label for="message">Message </label>
-        <textarea id="message" name="message"></textarea>
-        <button>Send</button>
-    </form>
-);
+                <div id="dates">
+                    <label for="start">Start date </label>
+                    <input type="date" id="start" name="start-date"
+                        value="2019-07-22"
+                        min="2019-07-01" max="2019-12-31" required></input>
+
+                    <label class="style-right" for="end">End date </label>
+                    <input type="date" id="end" name="end-date"
+                        value="2019-07-26"
+                        min="2019-07-01" max="2019-12-31" required></input>
+                </div>
+
+                <label for="message">Message </label>
+                <textarea id="message" name="message"></textarea>
+
+                <button>Send</button>
+            </form >
+        );
+    }
+}
 
 export default Form;
