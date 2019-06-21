d8c577be345b64433ac27f8000f7ad52e37027cd
Rado <43698977+BaiRado@users.noreply.github.com>
2019-06-21 16:19:26 +0100

    added comments

--- algorithms/sort/quicksort.js
+++ algorithms/sort/quicksort.js
@@ -1,14 +1,14 @@
 let test = [2, 1, 3, 5, 12.5, 15, 6, 54, 1, 567, -3, 32.2, 375, 53, 32.21, 9, -71, 57, 35, 73, 12, 123, 32, 2, 34];
 
-// function takes in arr to be sorted, and the first and last index of the part that we want partitioned
+// function takes in array to be sorted, as well as the first and last index of the part of the array that will be partitioned
 function partition(arr, first, last) {
     let pivot = arr[last], // last element is always used as a pivot
         temp,
-        c = first - 1; // c is the pos of the current element we are waiting to swap if we come across a number larger than the pivot
+        c = first - 1; // c is the pos of the current element that is waiting to be swapped if we come across a number larger than the pivot
 
     // loop through the part of the array we want partitioned
     for (let i = first; i <= last; i++) {
-        // if we find index bigger that the pivot swap it with c
+        // if we find a number smaller than the pivot swap it's index with c (moving smaller numbers to the left)
         if (arr[i] <= pivot) {
             c++;
             temp = arr[c];
@@ -16,16 +16,16 @@ function partition(arr, first, last) {
             arr[i] = temp;
         }
     }
-    // after the loop the pivot is in the correct position in the array and c is the index of the pivot in the array
+    // after the loop, the pivot is in it's final position in the array and c is the index, or location, of the pivot
     return c;
 }
 
 function quicksort(arr, first, last) {
     let pi;
 
-    // if arr is partitioned enough that the first and last index are the same number then stop the recursion
+    // if the partition is small enough so that the first and last index are the same number, then stop the recursion
     if (first < last) {
-        // pi is partitioning index or the index of the pivot the array was partitioned around
+        // pi is partitioning index, or the index of the pivot the array was partitioned around
         pi = partition(arr, first, last);
 
         quicksort(arr, first, pi - 1); // before pi
