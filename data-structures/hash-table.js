31825e36d55722bc56e7e4366cfc5de92d696819
Rado <43698977+BaiRado@users.noreply.github.com>
2019-06-24 17:18:35 +0100

    hash table using array buckets

--- data-structures/hash-table.js
+++ data-structures/hash-table.js
@@ -1,5 +1,4 @@
-// ES6 Hash Table
-const hash = (key, size) => {
+function hash(key, size) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
         hash += key.charCodeAt(i);
@@ -7,41 +6,62 @@ const hash = (key, size) => {
     return hash % size;
 }
 
-class HashTable {
-    constructor() {
-        this.size = 100;
-        this.storage = new Array(this.size);
+function HashTable() {
+    this.size = 20;
+    this.storage = new Array(this.size);
 
-        // Populate storage with Map
-        for (let i = 0; i < this.storage.length; i++) {
-            this.storage[i] = new Map()
+    this.add = function (key, value) {
+        let idx = hash(key, this.size);
+        // if there is already something in idx pos
+        if (this.storage[idx]) {
+            let inserted = false;
+            // check if key repeats / already exists
+            for (let i = 0; i < this.storage[idx].length; i++) {
+                if (this.storage[idx][i][0] === key) {
+                    // if key repeats, replace value
+                    this.storage[idx][i][1] = value;
+                    inserted = true;
+                }
+            }
+            // if key does NOT repeat, create new key/value pair
+            if (!inserted) {
+                this.storage[idx].push([key, value]);
+            }
+        } else {
+            // if there is nothing in idx pos, create new bucket and put key/value pair inside
+            this.storage[idx] = [
+                [key, value]
+            ];
         }
     }
 
-    add(key, value) {
+    this.remove = function (key) {
         let idx = hash(key, this.size);
-        this.storage[idx].set(key, value)
+        if (this.storage[idx]) {
+            for (let i = 0; i < this.storage[idx].length; i++) {
+                if (this.storage[idx][i][0] === key) {
+                    return this.storage[idx].splice(i, 1)
+                }
+            }
+        }
     }
 
-    delete(key) {
+    this.search = function (key) {
         let idx = hash(key, this.size);
-        let deleted = this.storage[idx].get(key);
-        this.storage[idx].delete(key);
-        return deleted;
-    }
-
-    search(key) {
-        let idx = hash(key, this.size);
-        return this.storage[idx].get(key);
+        if (this.storage[idx]) {
+            for (let i = 0; i < this.storage[idx].length; i++) {
+                if (this.storage[idx][i][0] === key) {
+                    return this.storage[idx][i][1];
+                }
+            }
+        }
     }
 }
 
-const hashTable = new HashTable;
-
-// Hash Table test:
+let hashTable = new HashTable();
 
 hashTable.add('Rado', 'Georgiev');
 // hashTable.search('Rado');
-// hashTable.delete('Rado');
+// hashTable.remove('Rado');
 
-console.log(hashTable);
+console.log(hashTable.storage);
