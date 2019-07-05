941d1fcc7578f9e0af583862df3b372966a513ed
Rado <43698977+BaiRado@users.noreply.github.com>
2019-07-05 21:49:40 +0100

    added comments

--- data-structures/linked-list.js
+++ data-structures/linked-list.js
@@ -14,39 +14,56 @@ class LinkedList {
         this.length = 0;
     }
 
+    // add Node at the end
     append(value) {
+        // if list is empty
         if (!this.head) {
+            // create first Node, which is both the head and tail of the list
             this.head = this.tail = new Node(value);
             this.length += 1;
         } else {
+            // store old tail in variable
             let old = this.tail;
+            // create new Node at the end of list and make it point to old tail
             this.tail = new Node(value, old, null);
+            // set old tail to point to new tail 
             old.next = this.tail;
             this.length += 1;
         }
     }
 
+    // add Node at the beginning
     prepend(value) {
+        // if list is empty
         if (!this.head) {
+            // create first Node, which is both the head and tail of the list
             this.head = this.tail = new Node(value);
             this.length += 1;
         } else {
+            // store old head in variable
             let old = this.head;
+            // create new Node at the start of list and make it point to old head
             this.head = new Node(value, null, old);
+            // set old head to point to new head 
             old.prev = this.head;
             this.length += 1;
         }
     }
 
+    // delete first Node
     deleteHead() {
+        // if list is empty
         if (!this.head) {
             return null;
         } else {
             let deleted = this.head;
 
+            // if it's the last Node in list (last Node is both head and tail of list)
             if (this.head === this.tail) {
                 this.head = this.tail = null;
-            } else {
+            }
+            // if there is more than 1 Node in list
+            else {
                 this.head = this.head.next;
                 this.head.prev = null;
             }
@@ -56,15 +73,20 @@ class LinkedList {
         }
     }
 
+    // delete last Node
     deleteTail() {
+        // if list is empty
         if (!this.head) {
             return null;
         } else {
             let deleted = this.tail;
 
+            // if it's the last Node in list (last Node is both head and tail of list)
             if (this.head === this.tail) {
                 this.head = this.tail = null;
-            } else {
+            }
+            // if there is more than 1 Node in list
+            else {
                 this.tail = this.tail.prev;
                 this.tail.next = null;
             }
@@ -74,18 +96,28 @@ class LinkedList {
         }
     }
 
-    deleteValue(value) {
+    // delete Node by value
+    deleteByValue(value) {
+        // if list is empty
         if (!this.head) {
             return null;
-        } else if (this.head.value === value) {
+        }
+        // if value is the same as first Node
+        else if (this.head.value === value) {
             return this.deleteHead();
-        } else if (this.tail.value === value) {
+        }
+        // if value is the same as last Node
+        else if (this.tail.value === value) {
             return this.deleteTail();
-        } else {
+        }
+        else {
+            // find value in list
             let deleted = this.searchVal(value);
 
+            // if value doesn't exists return null
             if (!deleted) return null;
 
+            // else delete Node by removing all references to it
             deleted.prev.next = deleted.next;
             deleted.next.prev = deleted.prev;
 
@@ -94,6 +126,7 @@ class LinkedList {
         }
     }
 
+    // delete Node by index
     deleteAtIdx(index) {
         // if linked list is empty or if index doesn't exist in linked list
         if (!this.head || index < 0 || index > this.length - 1) {
@@ -108,8 +141,10 @@ class LinkedList {
             return this.deleteTail();
         }
         else {
+            // find index in list
             let deleted = this.searchIdx(index);
 
+            // else delete Node by removing all references to it
             deleted.prev.next = deleted.next;
             deleted.next.prev = deleted.prev;
 
@@ -118,6 +153,7 @@ class LinkedList {
         }
     }
 
+    // search for Node by value
     searchVal(value) {
         // if linked list is empty
         if (!this.head) {
@@ -126,12 +162,14 @@ class LinkedList {
 
         let current = this.head;
 
+        // loop through list until Node with value is found
         while (current) {
             if (current.value === value) return current;
             current = current.next;
         }
     }
 
+    // search for Node by index
     searchIdx(index) {
         // if linked list is empty or if index doesn't exist in linked list
         if (!this.head || index < 0 || index > this.length - 1) {
@@ -141,6 +179,7 @@ class LinkedList {
         let current = this.head;
         let currentIndex = 0;
 
+        /// loop through list until Node with index is found
         while (current) {
             if (currentIndex === index) return current;
             currentIndex += 1;
