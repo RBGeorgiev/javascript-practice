7345a748dbb20f8db2311f841b7aee7b72588b04
Rado <43698977+BaiRado@users.noreply.github.com>
2019-07-07 15:28:10 +0100

    added add method

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -1,16 +1,34 @@
 class Node {
-    consturctor(char, end = false) {
-        this.char = char;
-        this.end = end;
-        this.child = new Map();
+    constructor() {
+        this.chars = new Map();
+        this.end = false;
     }
-
+    setEnd() {
+        this.end = true;
+    }
 }
 
 class Trie {
     constructor() {
         this.root = new Node();
     }
+
+    add(word, node = this.root) {
+        while (word.length > 0) {
+            if (!node.chars.get(word[0])) node.chars.set(word[0], new Node);
+            node = node.chars.get(word[0])
+            word = word.substring(1)
+        }
+        node.setEnd()
+    }
 }
 
 let trie = new Trie;
+trie.add('was')
+trie.add('were')
+trie.add('three')
+trie.add('tree')
+trie.add('to')
+trie.add('toga')
+trie.add('together')
+trie
