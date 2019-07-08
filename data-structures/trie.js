7c7182878f96bea0742bafccbe4d1a4dc776110e
Rado <43698977+BaiRado@users.noreply.github.com>
2019-07-08 13:17:53 +0100

    refactored and added comments

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -18,25 +18,41 @@ class Trie {
     }
 
     add(word) {
-        if (!word || typeof word !== 'string') return null;
+        // if not a string 
+        if (typeof word !== 'string') return null;
+
+        // start from root
         let node = this.root
+        // loop until no more letters in word
         while (word.length > 0) {
+            // if letter doesn't exists in Node, create new letter pointing to new Map (e.g. f => {})
             if (!node.chars.get(word[0])) node.chars.set(word[0], new Node);
+            // set current Node to letter 
             node = node.chars.get(word[0]);
+            // remove first character in string
             word = word.substring(1);
         }
+        // mark final letter as end of word
         node.setEnd();
     }
 
     exists(word) {
-        if (!word || typeof word !== 'string') return null;
+        // if not a string 
+        if (typeof word !== 'string') return null;
+
+        // start from root
         let node = this.root;
+        // loop through all letters in word
         while (word.length > 0) {
+            // if letter doesn't exists return false
             if (!node.chars.get(word[0])) return false;
+            // if letter exists set to current letter
             node = node.chars.get(word[0]);
+            // remove first character in string
             word = word.substring(1);
         }
-        return (word.length === 0 && node.isEnd())
+        // if last letter is marked as ending a word return true
+        return node.isEnd()
     }
 }
 
