ca3381882e9b8bdd0d24d734614ad86e1ab36e0e
Rado <43698977+BaiRado@users.noreply.github.com>
2019-07-08 11:45:47 +0100

    added exists method

--- data-structures/trie.js
+++ data-structures/trie.js
@@ -6,6 +6,10 @@ class Node {
     setEnd() {
         this.end = true;
     }
+
+    isEnd() {
+        return this.end;
+    }
 }
 
 class Trie {
@@ -13,22 +17,38 @@ class Trie {
         this.root = new Node();
     }
 
-    add(word, node = this.root) {
+    add(word) {
+        if (!word || typeof word !== 'string') return null;
+        let node = this.root
         while (word.length > 0) {
             if (!node.chars.get(word[0])) node.chars.set(word[0], new Node);
-            node = node.chars.get(word[0])
-            word = word.substring(1)
+            node = node.chars.get(word[0]);
+            word = word.substring(1);
         }
-        node.setEnd()
+        node.setEnd();
+    }
+
+    exists(word) {
+        if (!word || typeof word !== 'string') return null;
+        let node = this.root;
+        while (word.length > 0) {
+            if (!node.chars.get(word[0])) return false;
+            node = node.chars.get(word[0]);
+            word = word.substring(1);
+        }
+        return (word.length === 0 && node.isEnd())
     }
 }
 
 let trie = new Trie;
-trie.add('was')
-trie.add('were')
-trie.add('three')
-trie.add('tree')
-trie.add('to')
-trie.add('toga')
-trie.add('together')
+trie.add('was');
+trie.add('were');
+trie.add('three');
+trie.add('tree');
+trie.add('to');
+trie.add('toga');
+trie.add('together');
+
+trie.exists('three');
+trie.exists('motion');
 trie
