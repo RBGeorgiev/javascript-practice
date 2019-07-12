ef5588aa9df03fc6d8851fe039c689db8fcfde02
Rado <43698977+BaiRado@users.noreply.github.com>
2019-07-12 17:53:08 +0100

    added deleteVal

--- data-structures/min-heap.js
+++ data-structures/min-heap.js
@@ -8,6 +8,7 @@ function MinHeap() {
 
 MinHeap.prototype = {
     add: function (num) {
+        if (typeof num !== 'number') return null;
         // push num to the end of arr
         this.content.push(num);
         // get added element pos
@@ -19,14 +20,58 @@ MinHeap.prototype = {
             this.content[Math.floor(i / 2)] = temp;
             i = Math.floor(i / 2);
         }
+    },
+
+
+    // removes first value, doesn't remove duplicate numbers 
+    deleteVal: function (num) {
+        // if not a number
+        if (typeof num !== 'number') return null;
+
+        let idx = null;
+        for (let i = 1; i < this.content.length; i++) {
+            if (num === this.content[i]) {
+                idx = i;
+            }
+        }
+        if (!idx) return null;
+        let deleted = this.content[idx];
+        this.content[idx] = this.content.pop();
+
+        while (this.content[idx] < this.content[Math.floor(idx / 2)] && idx > 0) {
+            let temp = this.content[idx];
+            this.content[idx] = this.content[Math.floor(idx / 2)];
+            this.content[Math.floor(idx / 2)] = temp;
+            idx = Math.floor(idx / 2);
+        }
+
+        while (this.content[idx] > this.content[idx * 2] || this.content[idx] > this.content[idx * 2 + 1] && idx > 0) {
+            if (this.content[idx * 2] < this.content[idx * 2 + 1]) {
+                let temp = this.content[idx * 2];
+                this.content[idx * 2] = this.content[idx];
+                this.content[idx] = temp;
+                idx = idx * 2
+            }
+            else {
+                let temp = this.content[idx * 2 + 1];
+                this.content[idx * 2 + 1] = this.content[idx];
+                this.content[idx] = temp;
+                idx = idx * 2 + 1
+            }
+        }
+        return deleted;
     }
+
+    // sort
 }
 
 let minHeap = new MinHeap();
-minHeap.add(3)
+minHeap.add(3);
 
 let test = [1, 24, 13, 4, 55, 16, 7, 81, 324, 23, 42, 34]
 
 for (let i = 0; i < test.length; i++) {
-    minHeap.add(test[i])
-} 
+    minHeap.add(test[i]);
+}
+
+minHeap.content;
