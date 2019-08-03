2de28aab30ddd9fada1849854bae36c5f0312f20
Rado <43698977+BaiRado@users.noreply.github.com>
2019-08-03 22:19:32 +0100

    updated product cards style

--- shopping-cart-react/src/components/product.js
+++ shopping-cart-react/src/components/product.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 const Product = ({ product }) => {
     return (
         <article className="shelf-item-container">
+            {product.isFreeShipping && (
+                <div className="shelf-item-shipping">Free shipping</div>
+            )}
 
             <img src={require(`../img/${product.sku}.jpg`)} />
 
             <div className="shelf-item-name">{product.name}</div>
             <div className="shelf-item-price">{product.currencyFormat}{product.price.toFixed(2)}</div>
-            <span>{(product.isFreeShipping) ? 'Free Shipping' : ''}</span>
             <div className="shelf-item-buy-btn">Add to cart</div>
 
         </article>
