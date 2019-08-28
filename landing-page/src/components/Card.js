e31169b391fcd0f30b0102dcfe00ec99045ad143
Rado <43698977+BaiRado@users.noreply.github.com>
2019-08-28 16:14:15 +0100

    fixed errors

--- landing-page/src/components/Card.js
+++ landing-page/src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Card = (props) => (
     <section className={props.className}>
-        <img src={props.image} alt="info image" />
-        <div class="card-info">
+        <img src={props.image} alt="info" />
+        <div className="card-info">
             <h3>{props.title}</h3>
             <p>{props.info}</p>
         </div>
--- landing-page/src/components/Form.js
+++ landing-page/src/components/Form.js
@@ -11,22 +11,22 @@ class Form extends React.Component {
             <form onSubmit={this.onSubmit}>
                 <div id="contact-info">
                     <label>Name <input type="text" name="name" required></input></label>
-                    <label class="style-right">Email <input type="email" name="email" required></input></label>
+                    <label className="style-right">Email <input type="email" name="email" required></input></label>
                 </div >
 
                 <div id="dates">
-                    <label for="start">Start date </label>
+                    <label htmlFor="start">Start date </label>
                     <input type="date" id="start" name="start-date"
                         value="2019-07-22"
                         min="2019-07-01" max="2019-12-31" required></input>
 
-                    <label class="style-right" for="end">End date </label>
+                    <label className="style-right" htmlFor="end">End date </label>
                     <input type="date" id="end" name="end-date"
                         value="2019-07-26"
                         min="2019-07-01" max="2019-12-31" required></input>
                 </div>
 
-                <label for="message">Message </label>
+                <label htmlFor="message">Message </label>
                 <textarea id="message" name="message"></textarea>
 
                 <button>Send</button>
