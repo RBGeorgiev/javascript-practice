d5fcf8a2f247533d7fe566275dfdede289f84300
Rado <43698977+BaiRado@users.noreply.github.com>
2019-08-31 12:48:12 +0100

    fixed bug causing duplicate items in cart

--- shopping-cart-react/src/components/product-list/product-list.js
+++ shopping-cart-react/src/components/product-list/product-list.js
@@ -7,9 +7,22 @@ import { updateCart } from '../../store/update-cart/update-cart';
 
 class ProductList extends React.Component {
     addToCart(product) {
+        let idx,
+            inCart = false;
+
+        // Array.includes and Array.indexOf don't recognize cart items coming from local storage and thus create duplicates in the cart. 
+        // https://stackoverflow.com/a/14958115
+        // the for loop is a workaround/fix
+        for (let cartItem of this.props.cart) {
+            if (cartItem.sku === product.sku) {
+                inCart = true;
+                idx = this.props.cart.indexOf(cartItem);
+                break;
+            }
+        }
+
         // add item to cart or increase quantity if it's already present
-        if (this.props.cart.includes(product)) {
-            let idx = this.props.cart.indexOf(product);
+        if (inCart) {
             // stop adding quantity after 10
             if (this.props.cart[idx].quantity < 10) this.props.cart[idx].quantity++;
         } else {
