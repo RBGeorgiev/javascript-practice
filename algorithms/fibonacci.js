b51f20a313b4405ac9e861b758fea16273f69913
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-01 21:26:30 +0100

    added second method to generate fibonacci

--- algorithms/fibonacci.js
+++ algorithms/fibonacci.js
@@ -13,3 +13,12 @@ function fibonacci(num) {
 
     return ans;
 }
+
+
+function fibonacci_2(n) {
+    let arr = [0, 1];
+    for (let i = 1; i < n; i++) {
+        arr.push(arr[i - 1] + arr[i]);
+    }
+    return arr;
+}
