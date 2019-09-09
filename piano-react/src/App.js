3272400949e2dd98b1f425f1d8f4746462ed73ae
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-09 11:50:04 +0100

    added createKey method to Octave

--- piano-react/src/App.js
+++ piano-react/src/App.js
@@ -20,12 +20,20 @@ function Key(props) {
 function Octave({ pitch }) {
   const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 
+  const createKey = (n) =>
+    <Key
+      key={n + pitch}
+      note={n + pitch}
+      className={(n.length === 1) ? 'key' : 'key black'}
+    />
+
   const createOctave = (pitch) => {
-    if (pitch === 0) return notes.slice(-3).map(n => <Key key={n + pitch} note={n + pitch} className={(n.length === 1) ? 'key' : 'key black'} />);
-
-    if (pitch === 8) return notes.slice(0, 1).map(n => <Key key={n + pitch} note={n + pitch} className={(n.length === 1) ? 'key' : 'key black'} />);
-
-    return notes.map(n => <Key key={n + pitch} note={n + pitch} className={(n.length === 1) ? 'key' : 'key black'} />);
+    // pitch 0 has only 3 notes: 'A0', 'A#0', 'B0'
+    if (pitch === 0) return notes.slice(-3).map(n => createKey(n));
+    // pitch 8 has only 1 note: 'C8'
+    if (pitch === 8) return notes.slice(0, 1).map(n => createKey(n));
+    // all other pitches include all notes
+    return notes.map(n => createKey(n));
   }
 
   return (
