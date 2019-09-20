fbc253dc22d55f99fcd71e70d621bd67865550a6
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-20 16:15:17 +0100

    improved controls

--- racing-game/src/car.js
+++ racing-game/src/car.js
@@ -12,9 +12,13 @@ export default class Car {
             y: (this.game.gameHeight - this.size.height) / 2
         }
 
-        this.speed = 0.5;
+        this.speed = 0.8;
         this.mod = 0;
         this.angle = 0;
+        this.rotate = 0
+
+        document.addEventListener('keydown', (e) => keyDown_handler(e, this))
+        document.addEventListener('keyup', (e) => keyUp_handler(e, this))
     }
 
     draw(ctx) {
@@ -22,15 +26,51 @@ export default class Car {
         ctx.beginPath();
         ctx.translate(this.pos.x + this.size.width / 2, this.pos.y + this.size.height / 2);
         ctx.rotate(this.angle * Math.PI / 180);
-        ctx.rect(-this.size.width / 2, -this.size.height / 2, this.size.width, this.size.height);
+        ctx.rect(-this.size.width, -this.size.height / 2, this.size.width, this.size.height);
         ctx.fillStyle = 'red';
         ctx.fill();
         ctx.restore();
-
     }
 
     update(deltaTime) {
-        this.pos.x += (this.speed * this.mod) * Math.cos(Math.PI / 180 * this.angle) * deltaTime;
-        this.pos.y += (this.speed * this.mod) * Math.sin(Math.PI / 180 * this.angle) * deltaTime;
+        this.pos.x += (this.speed * this.mod) * Math.cos(Math.PI / 180 * (this.angle += this.rotate)) * deltaTime;
+        this.pos.y += (this.speed * this.mod) * Math.sin(Math.PI / 180 * (this.angle += this.rotate)) * deltaTime;
+    }
+}
+
+
+function keyUp_handler(e, car) {
+    switch (e.keyCode) {
+        case 38: //UP
+        case 40: //DOWN
+            car.mod = 0;
+            break;
+        case 37:
+            if (car.rotate < 0) car.rotate = 0;
+            break;
+        case 39:
+            if (car.rotate > 0) car.rotate = 0;
+            break;
+
+    }
+}
+
+function keyDown_handler(e, car) {
+    switch (e.keyCode) {
+        case 38: //UP
+            car.mod = 1;
+            break;
+
+        case 40: //DOWN
+            car.mod = -1;
+            break;
+
+        case 37: //LEFT
+            car.rotate = -3;
+            break;
+
+        case 39: //RIGHT
+            car.rotate = 3;
+            break;
     }
 }
--- racing-game/src/game.js
+++ racing-game/src/game.js
@@ -6,9 +6,6 @@ export default class Game {
         this.gameHeight = gameHeight;
 
         this.car = new Car(this);
-
-        document.addEventListener('keydown', (e) => keyDown_handler(e, this.car))
-        document.addEventListener('keyup', (e) => keyUp_handler(e, this.car))
     }
 
     draw(ctx) {
@@ -21,34 +18,5 @@ export default class Game {
 
     update(deltaTime) {
         this.car.update(deltaTime);
-    }
-}
-
-function keyUp_handler(e, car) {
-    switch (e.keyCode) {
-        case 38: //UP
-        case 40: //DOWN
-            car.mod = 0;
-    }
-}
-
-function keyDown_handler(e, car) {
-    console.log(e.keyCode)
-    switch (e.keyCode) {
-        case 38: //UP
-            car.mod = -1;
-            break;
-
-        case 40: //DOWN
-            car.mod = 1;
-            break;
-
-        case 37: //LEFT
-            car.angle -= 15;
-            break;
-
-        case 39: //RIGHT
-            car.angle += 15;
-            break;
     }
 }
