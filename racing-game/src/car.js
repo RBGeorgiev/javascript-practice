2683b83b89177e666211321950f9273f7abf43bc
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-20 13:54:18 +0100

    added movement to car

--- racing-game/src/car.js
+++ racing-game/src/car.js
@@ -1,22 +1,36 @@
 export default class Car {
     constructor(game) {
         this.size = {
-            width: 20,
-            height: 40
+            width: 40,
+            height: 20
         };
 
-        this.game = game
+        this.game = game;
 
         this.pos = {
             x: (this.game.gameWidth - this.size.width) / 2,
             y: (this.game.gameHeight - this.size.height) / 2
         }
+
+        this.speed = 0.5;
+        this.mod = 0;
+        this.angle = 0;
     }
 
     draw(ctx) {
+        ctx.save();
+        ctx.beginPath();
+        ctx.translate(this.pos.x + this.size.width / 2, this.pos.y + this.size.height / 2);
+        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.rect(-this.size.width / 2, -this.size.height / 2, this.size.width, this.size.height);
         ctx.fillStyle = 'red';
-        ctx.fillRect(this.pos.x, this.pos.y, this.size.width, this.size.height);
+        ctx.fill();
+        ctx.restore();
+
     }
 
-    update(deltaTime) { }
+    update(deltaTime) {
+        this.pos.x += (this.speed * this.mod) * Math.cos(Math.PI / 180 * this.angle) * deltaTime;
+        this.pos.y += (this.speed * this.mod) * Math.sin(Math.PI / 180 * this.angle) * deltaTime;
+    }
 }
--- racing-game/src/game.js
+++ racing-game/src/game.js
@@ -1,4 +1,4 @@
-import Car from './car.js'
+import Car from './car.js';
 
 export default class Game {
     constructor(gameWidth, gameHeight) {
@@ -6,8 +6,10 @@ export default class Game {
         this.gameHeight = gameHeight;
 
         this.car = new Car(this);
-    }
 
+        document.addEventListener('keydown', (e) => keyDown_handler(e, this.car))
+        document.addEventListener('keyup', (e) => keyUp_handler(e, this.car))
+    }
 
     draw(ctx) {
         ctx.fillStyle = 'green';
@@ -18,6 +20,35 @@ export default class Game {
     }
 
     update(deltaTime) {
+        this.car.update(deltaTime);
+    }
+}
 
+function keyUp_handler(e, car) {
+    switch (e.keyCode) {
+        case 38: //UP
+        case 40: //DOWN
+            car.mod = 0;
+    }
+}
+
+function keyDown_handler(e, car) {
+    console.log(e.keyCode)
+    switch (e.keyCode) {
+        case 38: //UP
+            car.mod = -1;
+            break;
+
+        case 40: //DOWN
+            car.mod = 1;
+            break;
+
+        case 37: //LEFT
+            car.angle -= 15;
+            break;
+
+        case 39: //RIGHT
+            car.angle += 15;
+            break;
     }
 }
