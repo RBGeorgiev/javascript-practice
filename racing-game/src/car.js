64a73612f0afb9e8cc604d9603a49ed4e60d1df9
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-21 16:06:41 +0100

    added drift to car

--- racing-game/src/car.js
+++ racing-game/src/car.js
@@ -1,8 +1,8 @@
 export default class Car {
     constructor(game) {
         this.size = {
-            width: 20,
-            height: 10
+            width: 40,
+            height: 20
         };
 
         this.game = game;
@@ -12,13 +12,17 @@ export default class Car {
             y: (this.game.gameHeight - this.size.height) / 2
         }
 
-        this.maxSpeed = 1
+        this.maxSpeed = 100
         this.speed = 0;
         this.acc = 0;
 
         this.mod = 0;
         this.angle = 0;
         this.rotate = 0
+
+        this.maxDrift = 14
+        this.drift = 0
+        this.driftAngle = 0
 
         document.addEventListener('keydown', (e) => keyDown_handler(e, this))
         document.addEventListener('keyup', (e) => keyUp_handler(e, this))
@@ -28,7 +32,7 @@ export default class Car {
         ctx.save();
         ctx.beginPath();
         ctx.translate(this.pos.x + this.size.width / 2, this.pos.y + this.size.height / 2);
-        ctx.rotate(this.angle * Math.PI / 180);
+        ctx.rotate(this.applyDrift() + this.angle * Math.PI / 180);
         ctx.rect(-this.size.width, -this.size.height / 2, this.size.width, this.size.height);
         ctx.fillStyle = 'red';
         ctx.fill();
@@ -36,19 +40,38 @@ export default class Car {
     }
 
     update(deltaTime) {
-        console.log(this.speed)
-        if (this.speed === 0) this.rotate = 0
+        console.log(this.speed, this.drift)
+        if (this.speed === 0) this.rotate = 0;
+        this.resetDrift()
         this.pos.x += (this.applyAcc() * this.mod) * Math.cos(Math.PI / 180 * (this.angle += this.rotate)) * deltaTime;
         this.pos.y += (this.applyAcc() * this.mod) * Math.sin(Math.PI / 180 * (this.angle += this.rotate)) * deltaTime;
     }
 
     applyAcc() {
         this.speed += this.acc;
-        this.speed = this.speedClamp(this.speed, 0, this.maxSpeed)
-        return this.speed;
+        this.speed = this.clamp(this.speed, 0, this.maxSpeed)
+        return this.speed / 100;
     }
 
-    speedClamp(number, min, max) {
+    applyDrift() {
+        this.drift += this.driftAngle
+        this.drift = this.clamp(this.drift, -this.maxDrift, this.maxDrift)
+        return this.drift / 10
+    }
+
+    resetDrift() {
+        if (this.rotate === 0) {
+            if (this.drift > 0) {
+                this.driftAngle = -1
+            } else if (this.drift < 0) {
+                this.driftAngle = 1
+            } else if (this.drift === 0) {
+                this.driftAngle = 0
+            }
+        }
+    }
+
+    clamp(number, min, max) {
         // caps min and max speed
         return Math.max(min, Math.min(number, max));
     }
@@ -59,15 +82,20 @@ function keyUp_handler(e, car) {
     switch (e.keyCode) {
         case 38: //UP
         case 40: //DOWN
-            (car.acc > 0) ? car.acc = -0.02 : car.mod = 0;
+            (car.acc > 0) ? car.acc = -2 : car.mod = 0;
             break;
         case 37:
-            if (car.rotate < 0) car.rotate = 0;
+            if (car.rotate < 0) {
+                car.rotate = 0;
+                car.driftAngle = 0
+            }
             break;
         case 39:
-            if (car.rotate > 0) car.rotate = 0;
+            if (car.rotate > 0) {
+                car.rotate = 0;
+                car.driftAngle = 0
+            }
             break;
-
     }
 }
 
@@ -76,23 +104,25 @@ function keyDown_handler(e, car) {
         case 38: //UP
             if (car.mod !== -1) {
                 car.mod = 1;
-                car.acc = 0.01
+                car.acc = 1
             }
             break;
 
         case 40: //DOWN
             if (car.mod !== 1) {
                 car.mod = -1;
-                car.acc = 0.01;
+                car.acc = 1;
             }
             break;
 
         case 37: //LEFT
             car.rotate = -3;
+            car.driftAngle = -1
             break;
 
         case 39: //RIGHT
             car.rotate = 3;
+            car.driftAngle = 1
             break;
     }
 }
