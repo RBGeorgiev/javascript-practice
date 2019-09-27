1aa69c40f2646e678926e38b75a35dfa2216ff74
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-27 19:09:27 +0100

    sensor collision now only finds closest collision

--- racing-game/src/game.js
+++ racing-game/src/game.js
@@ -22,7 +22,6 @@ export default class Game {
         this.map.draw(ctx);
         this.car.draw(ctx);
         this.drawCollisions(ctx)
-        // console.log(this.collisions)
     }
 
     update(deltaTime) {
@@ -34,35 +33,44 @@ export default class Game {
         let sens = this.car.sensors,
             outer = this.map.map.outerLines,
             inner = this.map.map.innerLines,
+            track = [...outer, ...inner],
             ans = [];
 
         for (let i = 0; i < sens.length; i++) {
-            for (let j = 0; j < outer.length; j++) {
+            let collisions = [];
+            for (let j = 0; j < track.length; j++) {
                 let cur = lineCollision(
                     sens[i].x1,
                     sens[i].y1,
                     sens[i].x2,
                     sens[i].y2,
-                    outer[j].x1,
-                    outer[j].y1,
-                    outer[j].x2,
-                    outer[j].y2
+                    track[j].x1,
+                    track[j].y1,
+                    track[j].x2,
+                    track[j].y2
                 )
-                if (cur) ans.push(cur)
+                if (cur) collisions.push(cur)
             }
-            for (let k = 0; k < inner.length; k++) {
-                let cur = lineCollision(
-                    sens[i].x1,
-                    sens[i].y1,
-                    sens[i].x2,
-                    sens[i].y2,
-                    inner[k].x1,
-                    inner[k].y1,
-                    inner[k].x2,
-                    inner[k].y2
-                )
-                if (cur) ans.push(cur)
+
+
+            let closest = [Infinity, null];
+
+            let sensX = sens[i].x1
+            let sensY = sens[i].y1
+            for (let k = 0; k < collisions.length; k++) {
+                let x = collisions[k].x - sensX
+                let y = collisions[k].y - sensY
+
+                x = Math.pow(x, 2)
+                y = Math.pow(y, 2)
+                let tot = Math.pow((x + y), 1 / 2)
+
+                if (closest[0] > tot) {
+                    closest[0] = tot
+                    closest[1] = { x: collisions[k].x, y: collisions[k].y }
+                }
             }
+            ans.push(closest[1])
         }
         this.collisions = ans;
     }
