c4e2f21d62ccf29dd2a863c92c90158104df836e
Rado <43698977+BaiRado@users.noreply.github.com>
2019-09-30 14:52:56 +0100

    moved car sensor collision logic to car class

--- racing-game/src/car.js
+++ racing-game/src/car.js
@@ -35,6 +35,8 @@ export default class Car {
         this.moving = 0;
         this.crashed = false;
 
+        this.sensorCollisions = [];
+
         document.addEventListener('keydown', (e) => keyDown_handler(e, this));
         document.addEventListener('keyup', (e) => keyUp_handler(e, this));
     }
@@ -272,6 +274,7 @@ export default class Car {
         ctx.lineWidth = 2;
         this.drawVertices(ctx);
         this.drawSensors(ctx);
+        this.drawSensorCollisions(ctx);
         this.drawSides(ctx);
     }
 
@@ -317,6 +320,74 @@ export default class Car {
             }
         }
         return this.crashed = false;
+    }
+
+    sensorTrackCollision(track) {
+        let sens = this.sensors,
+            ans = [];
+
+        // loop through each sensor
+        for (let i = 0; i < sens.length; i++) {
+            let collisions = [];
+
+            // find all collisions with sensor
+            for (let j = 0; j < track.length; j++) {
+                let cur = lineCollision(
+                    sens[i].x1,
+                    sens[i].y1,
+                    sens[i].x2,
+                    sens[i].y2,
+                    track[j].x1,
+                    track[j].y1,
+                    track[j].x2,
+                    track[j].y2
+                )
+                if (cur) collisions.push(cur)
+            }
+
+            // find closest collision
+            let closest = [Infinity, null];
+
+            for (let k = 0; k < collisions.length; k++) {
+                // find distance length from start of sensor to collision
+                let sensX = sens[i].x1,
+                    sensY = sens[i].y1,
+                    colX = collisions[k].x,
+                    colY = collisions[k].y,
+
+                    x = Math.pow(colX - sensX, 2),
+                    y = Math.pow(colY - sensY, 2),
+
+                    tot = Math.pow((x + y), 1 / 2);
+
+                // if distance is shorter than prev shortest distance
+                if (tot < closest[0]) {
+                    closest[0] = tot
+                    closest[1] = { x: collisions[k].x, y: collisions[k].y }
+                }
+            }
+            ans.push(closest[1])
+        }
+        this.sensorCollisions = ans;
+    }
+
+    drawSensorCollisions(ctx) {
+        let collisions = this.sensorCollisions,
+            sens = this.sensors[0];
+
+        for (let i = 0; i < collisions.length; i++) {
+            // Short-circuit evaluation fixes a game breaking error where collisions[i] returned null
+            let col = collisions[i] || sens,
+                x = col.x,
+                y = col.y;
+
+            ctx.beginPath();
+            ctx.arc(x, y, 3, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fillStyle = 'white'
+            ctx.font = "10px Arial";
+            ctx.fillText(`x: ${Math.round(x - sens.x1)}, y: ${Math.round(y - sens.y1)}`, x + 5, y - 5)
+        }
     }
 
     moveForward() {
--- racing-game/src/game.js
+++ racing-game/src/game.js
@@ -1,6 +1,5 @@
 import Car from './car.js';
 import Map from './map-draw.js';
-import lineCollision from './collision.js';
 
 
 export default class Game {
@@ -10,8 +9,6 @@ export default class Game {
 
         this.car = new Car(this);
         this.map = new Map;
-
-        this.sensorCollisions = [];
 
         this.track = [
             ...this.map.map.outerLines,
@@ -26,81 +23,11 @@ export default class Game {
 
         this.map.draw(ctx);
         this.car.draw(ctx);
-        this.drawSensorCollisions(ctx);
     }
 
     update(deltaTime) {
         this.car.update(deltaTime);
-        this.SensorTrackCollision();
+        this.car.sensorTrackCollision(this.track);
         this.car.carTrackCollision(this.track);
-    }
-
-    SensorTrackCollision() {
-        let sens = this.car.sensors,
-            track = this.track,
-            ans = [];
-
-        // loop through each sensor
-        for (let i = 0; i < sens.length; i++) {
-            let collisions = [];
-
-            // find all collisions with sensor
-            for (let j = 0; j < track.length; j++) {
-                let cur = lineCollision(
-                    sens[i].x1,
-                    sens[i].y1,
-                    sens[i].x2,
-                    sens[i].y2,
-                    track[j].x1,
-                    track[j].y1,
-                    track[j].x2,
-                    track[j].y2
-                )
-                if (cur) collisions.push(cur)
-            }
-
-            // find closest collision
-            let closest = [Infinity, null];
-
-            for (let k = 0; k < collisions.length; k++) {
-                // find distance length from start of sensor to collision
-                let sensX = sens[i].x1,
-                    sensY = sens[i].y1,
-                    colX = collisions[k].x,
-                    colY = collisions[k].y,
-
-                    x = Math.pow(colX - sensX, 2),
-                    y = Math.pow(colY - sensY, 2),
-
-                    tot = Math.pow((x + y), 1 / 2);
-
-                // if distance is shorter than prev shortest distance
-                if (tot < closest[0]) {
-                    closest[0] = tot
-                    closest[1] = { x: collisions[k].x, y: collisions[k].y }
-                }
-            }
-            ans.push(closest[1])
-        }
-        this.sensorCollisions = ans;
-    }
-
-    drawSensorCollisions(ctx) {
-        let collisions = this.sensorCollisions,
-            sens = this.car.sensors[0];
-
-        for (let i = 0; i < collisions.length; i++) {
-            // Short-circuit evaluation fixes a game breaking error where collisions[i] returned null
-            let col = collisions[i] || sens,
-                x = col.x,
-                y = col.y;
-
-            ctx.beginPath();
-            ctx.arc(x, y, 3, 0, 2 * Math.PI);
-            ctx.stroke();
-            ctx.fillStyle = 'white'
-            ctx.font = "10px Arial";
-            ctx.fillText(`x: ${Math.round(x - sens.x1)}, y: ${Math.round(y - sens.y1)}`, x + 5, y - 5)
-        }
     }
 }
