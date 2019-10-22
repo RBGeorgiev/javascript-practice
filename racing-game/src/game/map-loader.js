bf59378a1a25219081d4df7d4010de2c7a339a16
Rado <43698977+BaiRado@users.noreply.github.com>
2019-10-22 18:15:04 +0100

    refactored code

--- racing-game/src/game/map-loader.js
+++ racing-game/src/game/map-loader.js
@@ -15,32 +15,25 @@ export default class Map {
     }
 
     drawTrack(ctx) {
-        let outer = this.map.outerLines,
+        const outer = this.map.outerLines,
             inner = this.map.innerLines;
 
-        ctx.lineWidth = 2;
-        ctx.beginPath();
-        for (let i = 0; i < outer.length; i++) {
-            ctx.moveTo(outer[i].x1, outer[i].y1);
-            ctx.lineTo(outer[i].x2, outer[i].y2);
-        }
-        for (let i = 0; i < inner.length; i++) {
-            ctx.moveTo(inner[i].x1, inner[i].y1);
-            ctx.lineTo(inner[i].x2, inner[i].y2);
-        }
-        ctx.strokeStyle = "#000000";
-        ctx.stroke();
+        this.drawLines(ctx, outer);
+        this.drawLines(ctx, inner);
     }
 
     drawGates(ctx) {
-        let gates = this.map.gates;
+        const gates = this.map.gates;
+        this.drawLines(ctx, gates, "#18f051");
+    }
 
+    drawLines(ctx, lines, color = "#000000") {
         ctx.beginPath();
-        for (let i = 0; i < gates.length; i++) {
-            ctx.moveTo(gates[i].x1, gates[i].y1);
-            ctx.lineTo(gates[i].x2, gates[i].y2);
+        for (let i = 0; i < lines.length; i++) {
+            ctx.moveTo(lines[i].x1, lines[i].y1);
+            ctx.lineTo(lines[i].x2, lines[i].y2);
         }
-        ctx.strokeStyle = "#18f051";
+        ctx.strokeStyle = color;
         ctx.stroke();
     }
 }
