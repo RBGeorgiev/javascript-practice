274d174e994d77e4eb4423b0394e13ca2626380f
Rado <43698977+BaiRado@users.noreply.github.com>
2019-11-09 17:08:23 +0000

    added board

--- chess-react/src/App.js
+++ chess-react/src/App.js
@@ -1,10 +1,47 @@
 import React from 'react';
 import './App.css';
 
+function Square(props) {
+  return (
+    <div className={props.isWhite}>
+      {props.x}, {props.y}
+    </div>
+  )
+}
+
+
+function Board() {
+
+  const fillBoard = () => {
+    const board = [];
+
+    for (let y = 0; y < 8; y++) {
+      for (let x = 0; x < 8; x++) {
+        board.push(
+          <Square
+            key={y * 8 + x}
+            x={x}
+            y={y}
+            isWhite={((y + x) % 2) ? "square white" : "square black"}
+          />
+        )
+      }
+    }
+
+    return board;
+  }
+
+  return (
+    <div className="board">
+      {fillBoard()}
+    </div>
+  )
+}
+
 function App() {
   return (
-    <div className="App">
-      asdasdas
+    <div className="container">
+      <Board />
     </div>
   );
 }
