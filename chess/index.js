58483c50e21d300e40fac8cfbd8be71ededdd5c7
Rado <43698977+BaiRado@users.noreply.github.com>
2019-11-10 19:37:32 +0000

    added black squares and variable square/board size

--- chess/index.js
+++ chess/index.js
@@ -1,10 +1,23 @@
-let container = document.getElementsByClassName("container");
+function initBoard(squareSize) {
+    let board = document.getElementsByClassName("board");
 
-for (let x = 0; x < 8; x++) {
-    for (let y = 0; y < 8; y++) {
-        let square = document.createElement("div");
-        square.className = "square"
-        square.innerHTML = `${x}, ${y}`;
-        container[0].appendChild(square)
+    board[0].style.width = `${squareSize * 8}px`;
+    board[0].style.height = `${squareSize * 8}px`;
+
+    for (let x = 0; x < 8; x++) {
+        for (let y = 0; y < 8; y++) {
+            let square = document.createElement("div");
+
+            let className = "square";
+            if ((x + y) % 2 === 0) className += " sqBlack";
+            square.className = className;
+
+            square.style.width = `${squareSize}px`;
+            square.style.height = `${squareSize}px`;
+
+            board[0].appendChild(square);
+        }
     }
 }
+
+initBoard(70)
