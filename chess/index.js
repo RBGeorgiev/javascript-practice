c35e89f9c886e73bb7b3de576268146be6ef4b2c
Rado <43698977+BaiRado@users.noreply.github.com>
2019-11-12 14:27:53 +0000

    added available moves for all pieces

--- chess/index.js
+++ chess/index.js
@@ -1,11 +1,15 @@
+let boardSize = 8;
+let squareSize = 70;
+
 function initBoard(squareSize) {
     let board = document.getElementsByClassName("board");
 
-    board[0].style.width = `${squareSize * 8}px`;
-    board[0].style.height = `${squareSize * 8}px`;
 
-    for (let y = 0; y < 8; y++) {
-        for (let x = 0; x < 8; x++) {
+    board[0].style.width = `${squareSize * boardSize}px`;
+    board[0].style.height = `${squareSize * boardSize}px`;
+
+    for (let y = 0; y < boardSize; y++) {
+        for (let x = 0; x < boardSize; x++) {
             let square = document.createElement("div");
 
             let className = "square";
@@ -90,6 +94,8 @@ class King extends Piece {
     constructor(x, y, color) {
         super(x, y, color, "king");
 
+        this.hasMoved = false;
+
         this.pieceElem.piece = this;
 
         this.moves = this.moves();
@@ -100,7 +106,7 @@ class King extends Piece {
         let x = this.pos.x;
         let y = this.pos.y;
 
-        return [
+        let moves = [
             {
                 x: x - 1,
                 y: y
@@ -133,40 +139,236 @@ class King extends Piece {
             {
                 x: x + 1,
                 y: y + 1
-            },
+            }
         ]
+
+        return moves;
     }
 }
 
 class Queen extends Piece {
     constructor(x, y, color) {
         super(x, y, color, "queen");
+
+        this.pieceElem.piece = this;
+
+        this.moves = this.moves();
+        this.availableMoves = this.getAvailableMoves();
+    }
+
+    moves() {
+        let x = this.pos.x;
+        let y = this.pos.y;
+
+        let moves = [];
+
+        for (let idx = 0; idx < boardSize; idx++) {
+            moves.push(
+
+                {
+                    x: idx,
+                    y: y
+                },
+                {
+                    x: x,
+                    y: idx
+                },
+                {
+                    x: x + idx,
+                    y: y + idx
+                },
+                {
+                    x: x - idx,
+                    y: y + idx
+                },
+                {
+                    x: x + idx,
+                    y: y - idx
+                },
+                {
+                    x: x - idx,
+                    y: y - idx
+                }
+            )
+        }
+
+        return moves;
     }
 }
+
 class Rook extends Piece {
     constructor(x, y, color) {
         super(x, y, color, "rook");
+
+        this.hasMoved = false;
+
+        this.pieceElem.piece = this;
+
+        this.moves = this.moves();
+        this.availableMoves = this.getAvailableMoves();
+    }
+
+    moves() {
+        let x = this.pos.x;
+        let y = this.pos.y;
+
+        let moves = [];
+
+        for (let idx = 0; idx < boardSize; idx++) {
+            moves.push(
+                {
+                    x: idx,
+                    y: y
+                },
+                {
+                    x: x,
+                    y: idx
+                }
+            )
+        }
+
+        return moves;
     }
 }
+
 class Bishop extends Piece {
     constructor(x, y, color) {
         super(x, y, color, "bishop");
+
+        this.pieceElem.piece = this;
+
+        this.moves = this.moves();
+        this.availableMoves = this.getAvailableMoves();
+    }
+
+    moves() {
+        let x = this.pos.x;
+        let y = this.pos.y;
+
+        let moves = [];
+
+        for (let idx = 0; idx < boardSize; idx++) {
+            moves.push(
+                {
+                    x: x + idx,
+                    y: y + idx
+                },
+                {
+                    x: x - idx,
+                    y: y + idx
+                },
+                {
+                    x: x + idx,
+                    y: y - idx
+                },
+                {
+                    x: x - idx,
+                    y: y - idx
+                }
+            )
+        }
+
+        return moves;
     }
 }
 class Knight extends Piece {
     constructor(x, y, color) {
         super(x, y, color, "knight");
+
+        this.pieceElem.piece = this;
+
+        this.moves = this.moves();
+        this.availableMoves = this.getAvailableMoves();
+    }
+
+    moves() {
+        let x = this.pos.x;
+        let y = this.pos.y;
+
+        let moves = [
+            {
+                x: x + 2,
+                y: y - 1
+            },
+            {
+                x: x + 2,
+                y: y + 1
+            },
+            {
+                x: x - 2,
+                y: y - 1
+            },
+            {
+                x: x - 2,
+                y: y + 1
+            },
+            {
+                x: x + 1,
+                y: y + 2
+            },
+            {
+                x: x - 1,
+                y: y + 2
+            },
+            {
+                x: x + 1,
+                y: y - 2
+            },
+            {
+                x: x - 1,
+                y: y - 2
+            }
+        ]
+
+        return moves;
     }
 }
+
 class Pawn extends Piece {
     constructor(x, y, color) {
         super(x, y, color, "pawn");
+
+        this.hasMoved = false;
+
+        this.direction = (this.color === "white") ? 1 : -1;
+
+        this.pieceElem.piece = this;
+
+        this.moves = this.moves();
+        this.availableMoves = this.getAvailableMoves();
+
+    }
+
+    moves() {
+        let x = this.pos.x;
+        let y = this.pos.y;
+
+        let moves = [];
+
+        (this.hasMoved)
+            ?
+            moves.push({
+                x: x,
+                y: y + 1 * this.direction
+            })
+            :
+            moves.push(
+                {
+                    x: x,
+                    y: y + 1 * this.direction
+                },
+                {
+                    x: x,
+                    y: y + 2 * this.direction
+                });
+
+        return moves;
     }
 }
 
 function initPieces() {
     // white
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < boardSize; i++) {
         new Pawn(i, 1, "white");
     }
     new Rook(0, 0, "white");
@@ -179,7 +381,7 @@ function initPieces() {
     new Rook(7, 0, "white");
 
     // black
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < boardSize; i++) {
         new Pawn(i, 6, "black");
     }
     new Rook(0, 7, "black");
@@ -192,7 +394,7 @@ function initPieces() {
     new Rook(7, 7, "black");
 }
 
-initBoard(70);
+initBoard(squareSize);
 initPieces();
 
 document.addEventListener("mousedown", e => e.target.piece.colorMoves());
