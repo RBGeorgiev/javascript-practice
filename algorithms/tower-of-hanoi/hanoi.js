c8984fa1b86022b995635d2bc0a6c2445dcd641f
Rado <43698977+BaiRado@users.noreply.github.com>
2019-12-19 16:46:34 +0000

    added solutions for 4 and 5 pegs

--- algorithms/tower-of-hanoi/hanoi.js
+++ algorithms/tower-of-hanoi/hanoi.js
@@ -4,21 +4,60 @@ const displayMove = (str) => {
     answer.appendChild(move);
 }
 
-const move = (n, start, end) => {
-    let str = `Move ${n} from ${start} to ${end}`;
-    displayMove(str)
+const move = (num, start, end) => {
+    if (num < 1) return;
+    let str = `Move ${num} from ${start} to ${end}`;
+    displayMove(str);
 }
 
-const solveHanoi = (number, start, middle, end) => {
+const hanoiThreePegs = (number, start, middle, end) => {
     if (number < 1) return;
-    solveHanoi(number - 1, start, end, middle);
+    hanoiThreePegs(number - 1, start, end, middle);
     move(number, start, end);
-    solveHanoi(number - 1, middle, start, end);
+    hanoiThreePegs(number - 1, middle, start, end);
+}
+
+function hanoiFourPegs(num, start, mid1, mid2, end) {
+    if (num < 1) return;
+    hanoiFourPegs(num - 2, start, mid1, end, mid2);
+    move(num - 1, start, mid1);
+    move(num, start, end);
+    move(num - 1, mid1, end);
+    hanoiFourPegs(num - 2, mid2, start, mid1, end);
+}
+
+function hanoiFivePegs(num, start, mid1, mid2, mid3, end) {
+    if (num < 1) return;
+    hanoiFivePegs(num - 3, start, mid1, mid2, end, mid3)
+    move(num - 2, start, mid2);
+    move(num - 1, start, mid1);
+    move(num, start, end);
+    move(num - 1, mid1, end);
+    move(num - 2, mid2, end);
+    hanoiFivePegs(num - 3, mid3, start, mid1, mid2, end);
 }
 
 const getDiskNumber = () => diskNumber.value;
+const getPegNumber = () => pegNumber.value;
 
 calculateHanoi.onclick = () => {
     answer.innerHTML = "";
-    solveHanoi(getDiskNumber(), "A", "B", "C");
+    let disks = getDiskNumber();
+    let numPegs = getPegNumber();
+    let pegs = [];
+
+    for (let i = 0; i < numPegs; i++) {
+        pegs.push(String.fromCharCode(65 + i));
+    }
+
+    switch (numPegs) {
+        case "4":
+            hanoiFourPegs(disks, ...pegs);
+            break;
+        case "5":
+            hanoiFivePegs(disks, ...pegs);
+            break;
+        default:
+            hanoiThreePegs(disks, ...pegs);
+    }
 }
