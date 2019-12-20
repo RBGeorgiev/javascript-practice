220949c902b0142910301a40e57ad57462c173cd
Rado <43698977+BaiRado@users.noreply.github.com>
2019-12-20 12:38:23 +0000

    refactor

--- algorithms/tower-of-hanoi/hanoi.js
+++ algorithms/tower-of-hanoi/hanoi.js
@@ -10,54 +10,73 @@ const move = (num, start, end) => {
     displayMove(str);
 }
 
-const hanoiThreePegs = (number, start, middle, end) => {
-    if (number < 1) return;
-    hanoiThreePegs(number - 1, start, end, middle);
-    move(number, start, end);
-    hanoiThreePegs(number - 1, middle, start, end);
+class Hanoi {
+    ThreePegs = (number, start, middle, end) => {
+        if (number < 1) return;
+        this.ThreePegs(number - 1, start, end, middle);
+        move(number, start, end);
+        this.ThreePegs(number - 1, middle, start, end);
+    }
+
+    FourPegs = (num, start, mid1, mid2, end) => {
+        if (num < 1) return;
+        this.FourPegs(num - 2, start, mid1, end, mid2);
+        move(num - 1, start, mid1);
+        move(num, start, end);
+        move(num - 1, mid1, end);
+        this.FourPegs(num - 2, mid2, start, mid1, end);
+    }
+
+    FivePegs = (num, start, mid1, mid2, mid3, end) => {
+        if (num < 1) return;
+        this.FivePegs(num - 3, start, mid1, mid2, end, mid3)
+        move(num - 2, start, mid2);
+        move(num - 1, start, mid1);
+        move(num, start, end);
+        move(num - 1, mid1, end);
+        move(num - 2, mid2, end);
+        this.FivePegs(num - 3, mid3, start, mid1, mid2, end);
+    }
 }
 
-function hanoiFourPegs(num, start, mid1, mid2, end) {
-    if (num < 1) return;
-    hanoiFourPegs(num - 2, start, mid1, end, mid2);
-    move(num - 1, start, mid1);
-    move(num, start, end);
-    move(num - 1, mid1, end);
-    hanoiFourPegs(num - 2, mid2, start, mid1, end);
-}
+const hanoi = new Hanoi;
 
-function hanoiFivePegs(num, start, mid1, mid2, mid3, end) {
-    if (num < 1) return;
-    hanoiFivePegs(num - 3, start, mid1, mid2, end, mid3)
-    move(num - 2, start, mid2);
-    move(num - 1, start, mid1);
-    move(num, start, end);
-    move(num - 1, mid1, end);
-    move(num - 2, mid2, end);
-    hanoiFivePegs(num - 3, mid3, start, mid1, mid2, end);
+const clampNumber = (e) => {
+    let val = +e.target.value;
+    let min = +e.target.min;
+    let max = +e.target.max;
+
+    if (val < min) e.target.value = min;
+    if (val > max) e.target.value = max;
 }
 
 const getDiskNumber = () => diskNumber.value;
 const getPegNumber = () => pegNumber.value;
-
-calculateHanoi.onclick = () => {
-    answer.innerHTML = "";
-    let disks = getDiskNumber();
-    let numPegs = getPegNumber();
+const getPegs = (numPegs) => {
     let pegs = [];
-
     for (let i = 0; i < numPegs; i++) {
         pegs.push(String.fromCharCode(65 + i));
     }
+    return pegs;
+}
+
+const solveHanoi = () => {
+    answer.innerHTML = "";
+    let disks = getDiskNumber();
+    let numPegs = getPegNumber();
+    let pegs = getPegs(numPegs);
 
     switch (numPegs) {
         case "4":
-            hanoiFourPegs(disks, ...pegs);
+            hanoi.FourPegs(disks, ...pegs);
             break;
         case "5":
-            hanoiFivePegs(disks, ...pegs);
+            hanoi.FivePegs(disks, ...pegs);
             break;
         default:
-            hanoiThreePegs(disks, ...pegs);
+            hanoi.ThreePegs(disks, ...pegs);
     }
 }
+
+diskNumber.onchange = (e) => clampNumber(e);
+calculateHanoi.onclick = () => solveHanoi();
