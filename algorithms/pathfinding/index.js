f07c49d52da8be5152e36854d618e5663b61e720
Rado <43698977+BaiRado@users.noreply.github.com>
2020-01-20 17:56:11 +0000

    added tile types and tile colors

--- algorithms/pathfinding/index.js
+++ algorithms/pathfinding/index.js
@@ -4,6 +4,25 @@ let ctx = canvas.getContext("2d");
 canvas.width = 1600;
 canvas.height = 800;
 
+const TILE_COLORS = {
+    START: "lightgreen",
+    END: "red",
+    OPEN_LIST: "lightblue",
+    CLOSED_LIST: "blue",
+    PATH: "orange"
+}
+Object.freeze(TILE_COLORS);
+
+const TILE_TYPES = {
+    EMPTY: 0,
+    START: 1,
+    END: 2,
+    OPEN_LIST: 3,
+    CLOSED_LIST: 4,
+    PATH: 5
+}
+Object.freeze(TILE_TYPES);
+
 class TileMap {
     constructor() {
         this.columns = 100;
@@ -17,8 +36,8 @@ class TileMap {
         return this.tilemap[x][y];
     }
 
-    setTile = (x, y, val) => {
-        return this.tilemap[x][y] = val;
+    setTile = (x, y, type) => {
+        return this.tilemap[x][y] = type;
     }
 
     initTileMap = () => {
@@ -29,11 +48,13 @@ class TileMap {
 
     drawTile = (x, y, size, color) => {
         ctx.beginPath();
-        ctx.strokeStyle = "grey";
-        ctx.fillStyle = (color === 1) ? "green" : "white";
+
         ctx.lineWidth = 1;
+        ctx.strokeStyle = "grey";
         ctx.rect(x, y, size, size);
         ctx.stroke();
+
+        ctx.fillStyle = color;
         ctx.fill();
     }
 
@@ -43,10 +64,37 @@ class TileMap {
             for (let y = 0; y < this.tilemap[x].length; y++) {
                 let xPos = size * x;
                 let yPos = size * y;
-                let color = this.getTile(x, y);
+                let color = this.getTileColor(x, y);
                 this.drawTile(xPos, yPos, size, color);
             }
         }
+    }
+
+    getTileColor = (x, y) => {
+        let color;
+        let tile = this.getTile(x, y);
+
+        switch (tile) {
+            case 1:
+                color = TILE_COLORS.START;
+                break;
+            case 2:
+                color = TILE_COLORS.END;
+                break;
+            case 3:
+                color = TILE_COLORS.OPEN_LIST;
+                break;
+            case 4:
+                color = TILE_COLORS.CLOSED_LIST;
+                break;
+            case 5:
+                color = TILE_COLORS.PATH;
+                break;
+            default:
+                color = "white";
+        }
+
+        return color;
     }
 
     getTileFromCoordinates = (x, y) => {
