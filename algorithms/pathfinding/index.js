4b66ec1fe841240920f2206957d8aab74bdc6a1a
Rado <43698977+BaiRado@users.noreply.github.com>
2020-01-21 16:47:21 +0000

    code refactor

--- algorithms/pathfinding/index.js
+++ algorithms/pathfinding/index.js
@@ -4,49 +4,58 @@ let ctx = canvas.getContext("2d");
 canvas.width = 1600;
 canvas.height = 800;
 
-const TILE_COLORS = {
-    START: "lightgreen",
-    END: "red",
-    OPEN_LIST: "lightblue",
-    CLOSED_LIST: "blue",
-    PATH: "orange"
+// #region eNums
+const NODE_COLORS = {
+    EMPTY: "white",
+    UNWALKABLE: "black",
+    SWAMP: "burlywood"
 }
-Object.freeze(TILE_COLORS);
+Object.freeze(NODE_COLORS);
 
-const TILE_TYPES = {
-    EMPTY: 0,
-    START: 1,
-    END: 2,
-    OPEN_LIST: 3,
-    CLOSED_LIST: 4,
-    PATH: 5
+const NODE_TYPES = {
+    EMPTY: "EMPTY",
+    UNWALKABLE: 'UNWALKABLE',
+    SWAMP: "SWAMP"
 }
-Object.freeze(TILE_TYPES);
+Object.freeze(NODE_TYPES);
+// #endregion eNums
 
-class TileMap {
+
+class Node {
+    constructor(type = NODE_TYPES.EMPTY, isEnd = false) {
+        this.parent = null;
+        this.type = type;
+        this.isEnd = isEnd;
+    }
+}
+
+class Grid {
     constructor() {
         this.columns = 100;
         this.rows = 50;
-        this.tileSize = 16;
-        this.tilemap = [];
-        this.initTileMap();
+        this.gridSize = 16;
+        this.grid = [];
+        this.initGrid();
     }
 
-    getTile = (x, y) => {
-        return this.tilemap[x][y];
-    }
-
-    setTile = (x, y, type) => {
-        return this.tilemap[x][y] = type;
-    }
-
-    initTileMap = () => {
-        for (let i = 0; i < this.columns; i++) {
-            this.tilemap.push(Array(this.rows).fill(0));
+    initGrid = () => {
+        for (let x = 0; x < this.columns; x++) {
+            this.grid.push([]);
+            for (let y = 0; y < this.rows; y++) {
+                this.grid[x][y] = new Node;
+            }
         }
     }
 
-    drawTile = (x, y, size, color) => {
+    getNode = (x, y) => {
+        return this.grid[x][y];
+    }
+
+    setNodeType = (x, y, type) => {
+        return this.grid[x][y].type = type;
+    }
+
+    drawNode = (x, y, size, color) => {
         ctx.beginPath();
 
         ctx.lineWidth = 1;
@@ -58,82 +67,38 @@ class TileMap {
         ctx.fill();
     }
 
-    drawAllTiles = () => {
-        let size = this.tileSize;
-        for (let x = 0; x < this.tilemap.length; x++) {
-            for (let y = 0; y < this.tilemap[x].length; y++) {
+    drawAllNodes = () => {
+        let size = this.gridSize;
+        for (let x = 0; x < this.grid.length; x++) {
+            for (let y = 0; y < this.grid[x].length; y++) {
                 let xPos = size * x;
                 let yPos = size * y;
-                let color = this.getTileColor(x, y);
-                this.drawTile(xPos, yPos, size, color);
+                let color = this.getNodeColor(x, y);
+                this.drawNode(xPos, yPos, size, color);
             }
         }
     }
 
-    getTileColor = (x, y) => {
-        let color;
-        let tile = this.getTile(x, y);
-
-        switch (tile) {
-            case 1:
-                color = TILE_COLORS.START;
-                break;
-            case 2:
-                color = TILE_COLORS.END;
-                break;
-            case 3:
-                color = TILE_COLORS.OPEN_LIST;
-                break;
-            case 4:
-                color = TILE_COLORS.CLOSED_LIST;
-                break;
-            case 5:
-                color = TILE_COLORS.PATH;
-                break;
-            default:
-                color = "white";
-        }
-
-        return color;
+    getNodeColor = (x, y) => {
+        let node = this.getNode(x, y);
+        return NODE_COLORS[node.type];
     }
 
-    getTileFromCoordinates = (x, y) => {
+    getNodeFromCoordinates = (x, y) => {
         return {
-            x: Math.floor(x / this.tileSize),
-            y: Math.floor(y / this.tileSize)
+            x: Math.floor(x / this.gridSize),
+            y: Math.floor(y / this.gridSize)
         }
-    }
-}
-
-class Node {
-    constructor(x, y, parent = null, isEnd = false) {
-        this.x = x;
-        this.y = y;
-        this.parent = parent;
-        this.isEnd = isEnd;
     }
 }
 
 class AStar {
-    constructor(tilemap) {
-        this.tilemap = tilemap;
+    constructor(grid) {
+        this.grid = grid;
         this.startNode = null;
         this.endNode = null;
         this.openList = {};
         this.closedList = {};
-
-        this.setStartNode(5, 3);
-        this.setEndNode(65, 4);
-    }
-
-    setStartNode = (x, y) => {
-        this.startNode = new Node(x, y);
-        this.tilemap.setTile(x, y, TILE_TYPES.START);
-    }
-
-    setEndNode = (x, y) => {
-        this.endNode = new Node(x, y, null, true);
-        this.tilemap.setTile(x, y, TILE_TYPES.END);
     }
 
     getKey = (x, y) => {
@@ -141,9 +106,9 @@ class AStar {
     }
 }
 
-let tilemap = new TileMap;
-let aStar = new AStar(tilemap);
+let grid = new Grid;
+let aStar = new AStar(grid);
 
-tilemap.drawAllTiles();
+grid.drawAllNodes();
 
-canvas.addEventListener('click', (e) => tilemap.getTileFromCoordinates(e.offsetX, e.offsetY));
+canvas.addEventListener('click', (e) => grid.getNodeFromCoordinates(e.offsetX, e.offsetY));
