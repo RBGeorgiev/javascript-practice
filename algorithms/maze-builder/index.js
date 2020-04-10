1f84254faae095c1eb5c2f4ea7baba395a5a2efc
Rado <43698977+BaiRado@users.noreply.github.com>
2020-04-10 20:00:38 +0100

    canvas size now defined in html

--- algorithms/maze-builder/index.js
+++ algorithms/maze-builder/index.js
@@ -1,7 +1,3 @@
-import { canvas } from './src/constants.js';
 import Options from './src/options.js'
-
-canvas.width = 1600;
-canvas.height = 800;
 
 new Options;
--- algorithms/tower-of-hanoi/index.js
+++ algorithms/tower-of-hanoi/index.js
@@ -3,7 +3,4 @@ import Options from './src/options.js';
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-canvas.width = 1200;
-canvas.height = 600;
-
 new Options(ctx);
