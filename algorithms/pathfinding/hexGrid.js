60c8535d1f8ab0a88eb9c10565984e1bc2501bec
Rado <43698977+BaiRado@users.noreply.github.com>
2020-04-19 11:43:28 +0100

    refactored hex grid to work with nodes

--- algorithms/pathfinding/hexGrid.js
+++ algorithms/pathfinding/hexGrid.js
@@ -1,3 +1,5 @@
+import { Node } from './src/nodes.js';
+
 let canvas = document.getElementById("testCanvas");
 let ctx = canvas.getContext("2d");
 let gridWidth = 51;
@@ -11,7 +13,9 @@ canvas.style.border = "1px solid black";
 //     return [x + size * Math.cos(angle_rad), y + size * Math.sin(angle_rad)];
 // }
 
-flatHexCorner = (x, y, size, i) => {
+let grid = [];
+
+const flatHexCorner = (x, y, size, i) => {
     let angle_deg = 60 * i;
     let angle_rad = Math.PI / 180 * angle_deg;
     let cornerX = x + size * Math.cos(angle_rad);
@@ -19,25 +23,24 @@ flatHexCorner = (x, y, size, i) => {
     return (cornerX > canvas.width || cornerY > canvas.height) ? null : [cornerX, cornerY];
 }
 
-createHex = (x, y, size) => {
-    let points = [];
+const getHexCorners = (x, y, size) => {
+    let vertices = [];
 
     for (let i = 0; i < 6; i++) {
         let corner = flatHexCorner(x, y, size, i);
         if (corner === null) return null;
-        points.push(corner);
+        vertices.push(corner);
     }
 
-    return points;
+    return vertices;
 }
 
-drawHex = (points) => {
-    if (points === null) return;
+const drawHexNode = (vertices) => {
     for (let i = 0; i < 6; i++) {
-        let x = points[i][0];
-        let y = points[i][1];
-        let dx = points[(i + 1) % 6][0];
-        let dy = points[(i + 1) % 6][1];
+        let x = vertices[i][0];
+        let y = vertices[i][1];
+        let dx = vertices[(i + 1) % 6][0];
+        let dy = vertices[(i + 1) % 6][1];
 
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -46,17 +49,38 @@ drawHex = (points) => {
     }
 }
 
-for (let x = 0; x < gridWidth; x++) {
-    for (let y = 0; y < gridHeight; y++) {
-        let adjustedSize = size / 1.5;
+const initHexGrid = () => {
+    for (let x = 0; x < gridWidth; x++) {
+        for (let y = 0; y < gridHeight; y++) {
+            let adjustedSize = size / 1.5;
 
-        let w = 2 * adjustedSize;
-        let h = Math.sqrt(3) * adjustedSize;
-        let posX = (w * 3 / 4) * x
-        let posY = h * y;
-        let offset = (x % 2 === 1) ? h / 2 : 0;
+            let w = 2 * adjustedSize;
+            let h = Math.sqrt(3) * adjustedSize;
+            let posX = (w * 3 / 4) * x
+            let posY = h * y;
+            let offset = (x % 2 === 1) ? h / 2 : 0;
 
-        let points = createHex(posX + w / 2, posY + h / 2 + offset, adjustedSize);
-        drawHex(points);
+            let vertices = getHexCorners(posX + w / 2, posY + h / 2 + offset, adjustedSize);
+            if (vertices) {
+                if (!grid[x]) grid[x] = [];
+                let node = new Node(x, y);
+                node.vertices = vertices;
+                grid[x][y] = node;
+            }
+        }
     }
 }
+
+const drawAllHexNodes = () => {
+    let width = grid.length;
+    for (let x = 0; x < width; x++) {
+        let height = grid[x].length;
+        for (let y = 0; y < height; y++) {
+            let node = grid[x][y];
+            drawHexNode(node.vertices);
+        }
+    }
+}
+
+initHexGrid();
+drawAllHexNodes();
