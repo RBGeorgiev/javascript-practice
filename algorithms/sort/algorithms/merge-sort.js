0f748c967e6fcfac99fa95f84196c61c0dc194b0
Rado <43698977+BaiRado@users.noreply.github.com>
2020-06-14 14:03:26 +0100

    merge sort now visualizes correctly

--- algorithms/sort/algorithms/merge-sort.js
+++ algorithms/sort/algorithms/merge-sort.js
@@ -6,7 +6,7 @@ export default class MergeSort {
 
     addToStepsTaken = (arr) => this.stepsTaken.push([...arr]);
 
-    mergeSort(arr, prevStart = 0) {
+    mergeSort(arr, prevStartIdx = 0) {
         // if array length is 1 or 0
         if (arr.length < 2) return arr;
 
@@ -15,24 +15,23 @@ export default class MergeSort {
         let leftArr = arr.slice(0, middleIdx);
         let rightArr = arr.slice(middleIdx);
 
-        let mergedLeft = this.mergeSort(leftArr, prevStart);
-        let mergedRight = this.mergeSort(rightArr, middleIdx);
+        let mergedLeft = this.mergeSort(leftArr, prevStartIdx);
+        let mergedRight = this.mergeSort(rightArr, prevStartIdx + middleIdx);
 
-        return this.merge(mergedLeft, mergedRight, prevStart, middleIdx);
+        return this.merge(mergedLeft, mergedRight, prevStartIdx);
     }
 
-    merge(left, right, prevStart) {
+    merge(left, right, prevStartIdx) {
         let ans = [];
 
         while (left.length && right.length) {
             (left[0] < right[0]) ? ans.push(left.shift()) : ans.push(right.shift());
-            this.arr.splice(prevStart, ans.length, ...ans);
+            this.arr.splice(prevStartIdx, ans.length, ...ans);
             this.addToStepsTaken(this.arr);
         }
 
         ans = ans.concat(left, right);
-
-        this.arr.splice(prevStart, ans.length, ...ans);
+        this.arr.splice(prevStartIdx, ans.length, ...ans);
         this.addToStepsTaken(this.arr);
 
         return ans;
@@ -45,10 +44,3 @@ export default class MergeSort {
         this.mergeSort(arr);
     }
 }
-
-
-// this.arr.splice(prevMidIdx, mergedLeft.length, ...mergedLeft);
-// this.addToStepsTaken(this.arr);
-
-// this.arr.splice(curMidIdx, mergedRight.length, ...mergedRight);
-// this.addToStepsTaken(this.arr);
