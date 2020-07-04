036658133d5c77fc8e545ec19271c2419a141961
Rado <43698977+BaiRado@users.noreply.github.com>
2020-07-04 16:51:50 +0100

    added library for Voronoi diagrams

--- procedural-generation/index.js
+++ procedural-generation/index.js
@@ -1,3 +1,5 @@
+import Delaunator from './delaunator.js';
+
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -8,13 +10,13 @@ const generateRandomPoints = (amount, startX = 0, startY = 0, endX = canvas.widt
     for (let i = 0; i < amount; i++) {
         let x = Math.random() * (endX - startX) + startX;
         let y = Math.random() * (endY - startY) + startY;
-        allPoints.push({ x, y });
+        allPoints.push([x, y]);
     }
 
     return allPoints;
 }
 
-const displayPoints = allPoints => allPoints.forEach(p => ctx.fillRect(p.x, p.y, 3, 3));
+const displayPoints = allPoints => allPoints.forEach(p => ctx.fillRect(p[0], p[1], 3, 3));
 
 let allPoints = generateRandomPoints(1000);
 displayPoints(allPoints);
