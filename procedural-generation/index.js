2ac0f648bb71884f909416ae1155b3fa9ed83369
Rado <43698977+BaiRado@users.noreply.github.com>
2020-07-12 12:57:16 +0100

    map generator class

--- procedural-generation/index.js
+++ procedural-generation/index.js
@@ -3,106 +3,111 @@ import { Delaunay } from "./d3-delaunay/index.js";
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const generateRandomPoints = (amount, startX = 0, startY = 0, endX = canvas.width, endY = canvas.height) => {
-    let allPoints = [];
-    ctx.fillStyle = '#000000';
-
-    for (let i = 0; i < amount; i++) {
-        let x = Math.random() * (endX - startX) + startX;
-        let y = Math.random() * (endY - startY) + startY;
-        allPoints.push([x, y]);
+class MapGenerator {
+    constructor(numOfPoints) {
+        this.allPoints = generateRandomPoints(numOfPoints);
+        this.delaunay;
+        this.voronoi;
+        this.allVoronoiPolygonPoints;
+        init();
+        drawAll();
     }
 
-    return allPoints;
-}
+    lloydRelaxation = (points) => {
+        let len = points.length;
+        let coords = [];
 
-const drawPoints = points => points.forEach(p => ctx.fillRect(p[0], p[1], 3, 3));
+        for (let i = 0; i < len; i++) {
+            let polygonPoints = allVoronoiPolygonPoints[i];
+            if (!polygonPoints) continue;
+            let centroid = getCentroid(polygonPoints);
+            coords.push(centroid);
+        }
 
-const getAllVoronoiPolygonPoints = () => {
-    let len = allPoints.length;
-    let arr = [];
-
-    for (let i = 0; i < len; i++) {
-        arr.push(voronoi.cellPolygon(i));
+        return coords;
     }
 
-    return arr;
-}
-
-const getCentroid = (polygonPoints) => {
-    let totalX = 0;
-    let totalY = 0;
-    let len = polygonPoints.length;
-
-    for (let i = 0; i < len; i++) {
-        let x = polygonPoints[i][0];
-        let y = polygonPoints[i][1];
-
-        totalX += x;
-        totalY += y;
+    clearCanvas = () => {
+        ctx.beginPath();
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.closePath();
     }
 
-    return [totalX / len, totalY / len];
-}
-
-const lloydRelaxation = (points) => {
-    let len = points.length;
-    let coords = [];
-
-    for (let i = 0; i < len; i++) {
-        let polygonPoints = allVoronoiPolygonPoints[i];
-        if (!polygonPoints) continue;
-        let centroid = getCentroid(polygonPoints);
-        coords.push(centroid);
+    createVoronoi = (points) => {
+        delaunay = Delaunay.from(points);
+        voronoi = delaunay.voronoi([0, 0, canvas.width, canvas.height]);
+        allVoronoiPolygonPoints = getAllVoronoiPolygonPoints();
     }
 
-    return coords;
-}
+    drawDelaunay = () => {
+        ctx.strokeStyle = "red";
+        delaunay.render(ctx);
+        ctx.stroke();
+    }
 
-const clearCanvas = () => {
-    ctx.beginPath();
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.closePath();
-}
+    drawVoronoi = () => {
+        ctx.strokeStyle = "blue";
+        voronoi.render(ctx);
+        ctx.stroke();
+    }
 
-const createVoronoi = (points) => {
-    delaunay = Delaunay.from(points);
-    voronoi = delaunay.voronoi([0, 0, canvas.width, canvas.height]);
-    allVoronoiPolygonPoints = getAllVoronoiPolygonPoints();
-}
+    drawAll = () => {
+        clearCanvas();
+        drawVoronoi();
+        drawDelaunay()
+        drawPoints(allPoints);
+    }
 
-const drawDelaunay = () => {
-    ctx.strokeStyle = "red";
-    delaunay.render(ctx);
-    ctx.stroke();
-}
-
-const drawVoronoi = () => {
-    ctx.strokeStyle = "blue";
-    voronoi.render(ctx);
-    ctx.stroke();
-}
-
-const drawAll = () => {
-    clearCanvas();
-    drawVoronoi();
-    drawDelaunay()
-    drawPoints(allPoints);
-}
-
-const init = () => {
-    createVoronoi(allPoints);
-    relaxVoronoi(5);
-}
-
-const relaxVoronoi = (times) => {
-    for (let i = 0; i < times; i++) {
-        allPoints = lloydRelaxation(allPoints);
+    init = () => {
         createVoronoi(allPoints);
+        relaxVoronoi(5);
+    }
+
+    relaxVoronoi = (times) => {
+        for (let i = 0; i < times; i++) {
+            allPoints = lloydRelaxation(allPoints);
+            createVoronoi(allPoints);
+        }
+    }
+    generateRandomPoints = (amount, startX = 0, startY = 0, endX = canvas.width, endY = canvas.height) => {
+        let allPoints = [];
+        ctx.fillStyle = '#000000';
+
+        for (let i = 0; i < amount; i++) {
+            let x = Math.random() * (endX - startX) + startX;
+            let y = Math.random() * (endY - startY) + startY;
+            allPoints.push([x, y]);
+        }
+
+        return allPoints;
+    }
+
+    drawPoints = points => points.forEach(p => ctx.fillRect(p[0], p[1], 3, 3));
+
+    getAllVoronoiPolygonPoints = () => {
+        let len = allPoints.length;
+        let arr = [];
+
+        for (let i = 0; i < len; i++) {
+            arr.push(voronoi.cellPolygon(i));
+        }
+
+        return arr;
+    }
+
+    getCentroid = (polygonPoints) => {
+        let totalX = 0;
+        let totalY = 0;
+        let len = polygonPoints.length;
+
+        for (let i = 0; i < len; i++) {
+            let x = polygonPoints[i][0];
+            let y = polygonPoints[i][1];
+
+            totalX += x;
+            totalY += y;
+        }
+
+        return [totalX / len, totalY / len];
     }
 }
-
-let allPoints = generateRandomPoints(1000);
-let delaunay, voronoi, allVoronoiPolygonPoints;
-init();
-drawAll();
