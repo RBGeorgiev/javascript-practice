f190f03e41ebdf05aec3ad18cc905486259c08a0
Rado <43698977+BaiRado@users.noreply.github.com>
2020-08-06 20:09:55 +0100

    fixed infinite loop bug

--- procedural-generation/index.js
+++ procedural-generation/index.js
@@ -689,6 +689,9 @@ canvas.addEventListener("click", (e) => {
                 let top = tile.river;
                 let bot = lowestNeighbor.river;
 
+                if (bot.getRoot().idx === top.getRoot().idx) {
+                    continue;
+                }
                 top.setParent(bot);
                 bot.addChild(top);
 
