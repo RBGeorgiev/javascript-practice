37ae0c5640b1c26dd4e1344c61b8f438d58b455b
Rado <43698977+BaiRado@users.noreply.github.com>
2020-08-17 11:40:45 +0100

    added seeded random number generator

--- procedural-generation/index.js
+++ procedural-generation/index.js
@@ -945,4 +945,24 @@ canvas.addEventListener("click", (e) => {
     drawRiversOnVoronoiEdges(rivers);
     drawLakes();
     console.timeEnd("calculate wind precipitation rivers and lakes");
+
+
+
+
+    function sfc32(a, b, c, d) {
+        return function () {
+            a |= 0; b |= 0; c |= 0; d |= 0;
+            var t = (a + b | 0) + d | 0;
+            d = d + 1 | 0;
+            a = b ^ b >>> 9;
+            b = c + (c << 3) | 0;
+            c = c << 21 | c >>> 11;
+            c = c + t | 0;
+            return (t >>> 0) / 4294967296;
+        }
+    }
+
+    let seed = 2284314495;
+    let rng = sfc32(seed, seed, seed, seed);
+    for (let i = 0; i < 15; i++) console.log(rng())
 })
