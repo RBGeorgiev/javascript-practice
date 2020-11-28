b0cb05290521eb7abe1c8fe7bf8b426f0ec4b4cd
Rado <43698977+BaiRado@users.noreply.github.com>
2020-11-28 13:44:34 +0000

    added new constants

--- procedural-generation/constants.js
+++ procedural-generation/constants.js
@@ -1,2 +1,14 @@
-export const canvas = document.getElementById('canvas');
+const id = (id) => document.getElementById(id);
+
+export const canvas = id('canvas');
 export const ctx = canvas.getContext('2d');
+
+export const temperatureLabel = id('temperatureLabel');
+export const temperatureSpan = id('temperatureSpan');
+export const temperatureInput = id('temperatureInput');
+export const relativeHumidityLabel = id('relativeHumidityLabel');
+export const relativeHumiditySpan = id('relativeHumiditySpan');
+export const relativeHumidityInput = id('relativeHumidityInput');
+export const dewpointLabel = id('dewpointLabel');
+export const dewpointSpan = id('dewpointSpan');
+export const dewpointInput = id('dewpointInput');
