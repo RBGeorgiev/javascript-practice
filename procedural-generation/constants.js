f61d5963c3f2879502d0b908ac1a00d81cc74e95
Rado <43698977+BaiRado@users.noreply.github.com>
2020-12-26 13:13:19 +0000

    cleanup

--- procedural-generation/constants.js
+++ procedural-generation/constants.js
@@ -3,48 +3,34 @@ const id = (id) => document.getElementById(id);
 export const canvas = id('canvas');
 export const ctx = canvas.getContext('2d');
 
-export const temperatureLabel = id('temperatureLabel');
 export const temperatureSpan = id('temperatureSpan');
 export const temperatureInput = id('temperatureInput');
-export const relativeHumidityLabel = id('relativeHumidityLabel');
 export const relativeHumiditySpan = id('relativeHumiditySpan');
 export const relativeHumidityInput = id('relativeHumidityInput');
-export const dewpointLabel = id('dewpointLabel');
 export const dewpointSpan = id('dewpointSpan');
 export const dewpointInput = id('dewpointInput');
-export const windSpeedLabel = id('windSpeedLabel');
 export const windSpeedSpan = id('windSpeedSpan');
 export const windSpeedInput = id('windSpeedInput');
-export const precipitationForRiverMinLabel = id('precipitationForRiverMinLabel');
 export const precipitationForRiverMinSpan = id('precipitationForRiverMinSpan');
 export const precipitationForRiverMinInput = id('precipitationForRiverMinInput');
-export const precipitationForRiverMaxLabel = id('precipitationForRiverMaxLabel');
 export const precipitationForRiverMaxSpan = id('precipitationForRiverMaxSpan');
 export const precipitationForRiverMaxInput = id('precipitationForRiverMaxInput');
-export const precipitationForLakeMinLabel = id('precipitationForLakeMinLabel');
 export const precipitationForLakeMinSpan = id('precipitationForLakeMinSpan');
 export const precipitationForLakeMinInput = id('precipitationForLakeMinInput');
-export const precipitationForLakeMaxLabel = id('precipitationForLakeMaxLabel');
 export const precipitationForLakeMaxSpan = id('precipitationForLakeMaxSpan');
 export const precipitationForLakeMaxInput = id('precipitationForLakeMaxInput');
 
-export const heightPrecipitationMultiplierLabel = id('heightPrecipitationMultiplierLabel');
 export const heightPrecipitationMultiplierSpan = id('heightPrecipitationMultiplierSpan');
 export const heightPrecipitationMultiplierInput = id('heightPrecipitationMultiplierInput');
-export const lakeHeightPrecipitationMultiplierLabel = id('lakeHeightPrecipitationMultiplierLabel');
 export const lakeHeightPrecipitationMultiplierSpan = id('lakeHeightPrecipitationMultiplierSpan');
 export const lakeHeightPrecipitationMultiplierInput = id('lakeHeightPrecipitationMultiplierInput');
-export const riverWidthDistanceStrengthControlLabel = id('riverWidthDistanceStrengthControlLabel');
 export const riverWidthDistanceStrengthControlSpan = id('riverWidthDistanceStrengthControlSpan');
 export const riverWidthDistanceStrengthControlInput = id('riverWidthDistanceStrengthControlInput');
 
-export const heightDecrementMinLabel = id('heightDecrementMinLabel');
 export const heightDecrementMinSpan = id('heightDecrementMinSpan');
 export const heightDecrementMinInput = id('heightDecrementMinInput');
-export const heightDecrementMaxLabel = id('heightDecrementMaxLabel');
 export const heightDecrementMaxSpan = id('heightDecrementMaxSpan');
 export const heightDecrementMaxInput = id('heightDecrementMaxInput');
 
-export const chanceForLandLabel = id('chanceForLandLabel');
 export const chanceForLandSpan = id('chanceForLandSpan');
 export const chanceForLandInput = id('chanceForLandInput');
