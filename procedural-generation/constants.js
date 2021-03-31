91df060dc6242ea9a2ff34b67c1d10e65908d78d
Rado <43698977+BaiRado@users.noreply.github.com>
2021-03-31 12:45:40 +0100

    update

--- procedural-generation/constants.js
+++ procedural-generation/constants.js
@@ -85,6 +85,7 @@ export const displayTileCurrentPrecipitationValuesRadioBtn = id('displayTileCurr
 export const displayTileTotalPrecipitationValuesRadioBtn = id('displayTileTotalPrecipitationValuesRadioBtn');
 
 
+export const climateOptionsContainer = id('climateOptionsContainer');
 export const precipitationOptionsContainer = id('precipitationOptionsContainer');
 export const extraOptionsContainer = id('extraOptionsContainer');
 export const advancedOptionsContainer = id('advancedOptionsContainer');
