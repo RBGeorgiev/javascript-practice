b68027ce6c3d0bf80f301606b49dd3122f2aac5a
Rado <43698977+BaiRado@users.noreply.github.com>
2021-06-04 15:28:41 +0100

    revised deepest depth text

--- procedural-generation/index.js
+++ procedural-generation/index.js
@@ -1934,7 +1934,7 @@ for (let div of htmlDropdowns) {
 
 function updateHtmlDisplayedValues() {
     highestPeakSpan.innerText = `${mapGen.highestPeak * 10}m`;
-    deepestDepthSpan.innerText = mapGen.deepestDepth + '0m';
+    deepestDepthSpan.innerText = `${mapGen.deepestDepth * 10}m`;
     longestRiverSpan.innerText = mapGen.longestRiverLength + '0m';
     tileInfoDiv.innerHTML = '';
 }
