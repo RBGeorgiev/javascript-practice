83f14b12074d58ce992b9d4ce50abcf265100a57
Rado <43698977+BaiRado@users.noreply.github.com>
2021-10-06 16:04:08 +0100

    biome count is now sorted in html

--- procedural-generation/index.js
+++ procedural-generation/index.js
@@ -1972,7 +1972,8 @@ function updateHtmlDisplayedValues() {
     for (let biome in mapGen.biomeCount) {
         sortedBiomes.push([biome, mapGen.biomeCount[biome]]);
     }
-    sortedBiomes.sort((a, b) => a[1] - b[1]);
+    sortedBiomes.sort((a, b) => a[1] - b[1]).forEach((biome => biomeCountDiv.innerHTML += `<span class="biomeCountBiome">${biome[0]}:</span> <span class="biomeCountValue">${biome[1]}</span>`)); // display biome count in
+    console.log(sortedBiomes);
 }
 
 
