55648c84981e6f8ee61803fd821426dceccab2a2
Rado <43698977+BaiRado@users.noreply.github.com>
2021-11-17 16:09:15 +0000

    added className

--- chat-app-react/src/components/ChatBar.jsx
+++ chat-app-react/src/components/ChatBar.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export const ChatBar = () => {
     const [input, setInput] = useState('');
     return (        
-        <input value={input} onInput={e => setInput(e.target.value)}/>
+        <input className="chatBar" value={input} onInput={e => setInput(e.target.value)}/>
     )
 }
 
