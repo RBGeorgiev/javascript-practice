d1f37d5a76641e545f4a2cc37cc5744e85b46bc7
Rado <43698977+BaiRado@users.noreply.github.com>
2021-11-25 14:24:14 +0000

    fixed formatting

--- chat-app-react/src/components/ChatBar.jsx
+++ chat-app-react/src/components/ChatBar.jsx
@@ -1,27 +1,30 @@
-import { useState, useRef } from 'react';
+import { useState, useRef } from "react";
 
 export const ChatBar = (props) => {
-    const [input, setInput] = useState('');
-    const chatBarInput = useRef(null);
+	const [input, setInput] = useState("");
+	const chatBarInput = useRef(null);
 
-    const resetBar = (e) => {
-        setInput('');
-        e.target.value = input;
-        chatBarInput.current.focus();
-    }
+	const resetBar = (e) => {
+		setInput("");
+		e.target.value = input;
+		chatBarInput.current.focus();
+	};
 
-    const submitMessage = (e) => {
-        e.preventDefault();
-        props.addMessage(input);
-        resetBar(e);
-    }
+	const submitMessage = (e) => {
+		e.preventDefault();
+		props.addMessage(input);
+		resetBar(e);
+	};
 
-    return (
-        <form className="chatBarContainer" onSubmit={e => submitMessage(e)}>
-            <input id="chatBar" ref={chatBarInput} value={input} onInput={e => setInput(e.target.value)} />
-            <input type="submit" htmlFor="chatBar" value="Submit" />
-        </form>
-    )
-}
-
-
+	return (
+		<form className="chatBarContainer" onSubmit={(e) => submitMessage(e)}>
+			<input
+				id="chatBar"
+				ref={chatBarInput}
+				value={input}
+				onInput={(e) => setInput(e.target.value)}
+			/>
+			<input type="submit" htmlFor="chatBar" value="Submit" />
+		</form>
+	);
+};
