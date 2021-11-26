d55cccd89a76eb0535c084938055378515e5f07a
Rado <43698977+BaiRado@users.noreply.github.com>
2021-11-26 16:46:21 +0000

    formatting

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import './sanitize.css';
-import './App.css';
-import { ChatContainer } from './components/ChatContainer.jsx';
+import React from "react";
+import "./sanitize.css";
+import "./App.css";
+import { ChatContainer } from "./components/ChatContainer.jsx";
 
 function App() {
-  return (
-    <div>
-      <ChatContainer />
-    </div>
-  );
+	return (
+		<div>
+			<ChatContainer />
+		</div>
+	);
 }
 
 export default App;
