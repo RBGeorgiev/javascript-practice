01aa7d57ac67217d9627ca29eab1681f54197eae
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-06 14:58:22 +0000

    added useState hook

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sanitize.css";
 import "./App.css";
 import { ChatContainer } from "./components/ChatContainer.jsx";
 
 function App() {
+	const [test, setTest] = useState("test");
 	return (
 		<div>
 			<ChatContainer />
+			<div>{test}</div>
 		</div>
 	);
 }
