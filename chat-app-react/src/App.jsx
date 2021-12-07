ead42f99ed750e87fff2f96a4b8010546ccbe518
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-07 19:50:22 +0000

    useEffect hook

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./sanitize.css";
 import "./App.css";
 import { ChatContainer } from "./components/ChatContainer.jsx";
 
 function App() {
 	const [test, setTest] = useState("test");
+
+	useEffect(() => {}, []);
+
 	return (
 		<div>
 			<ChatContainer />
