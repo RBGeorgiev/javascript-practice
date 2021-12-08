e0033ae36cf800e78a685b249cb8c2dcd1a5e2b5
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-08 17:32:06 +0000

    backend response saved

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
 	useEffect(() => {
 		async function getTest() {
-			let response = await fetch("/test").then((res) => res.json());
+			let response = await fetch("/test")
+				.then((res) => res.json())
+				.then((t) => setTest(t))
+				.catch((err) => console.log(err));
 			console.log(response);
 		}
 		getTest();
