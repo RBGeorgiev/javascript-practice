f7bcf04055351c809a8eb89790e5e8da8f8ed63d
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-15 17:24:55 +0000

    removed test

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -4,27 +4,10 @@ import "./App.css";
 import { ChatContainer } from "./components/ChatContainer.jsx";
 
 function App() {
-    const [test, setTest] = useState([]);
-
-    const getTest = async () =>
-        await fetch("http://localhost:5000/")
-            .then((res) => res.json())
-            .then((t) => setTest(t))
-            .catch((err) => console.log(err));
-
-    useEffect(() => getTest(), []);
-
     return (
         <div>
             <h1>Chat App React</h1>
             <ChatContainer />
-            <div>
-                {test.map((el) => (
-                    <p key={el.id}>
-                        {el.id}: {el.nickname}
-                    </p>
-                ))}
-            </div>
         </div>
     );
 }
