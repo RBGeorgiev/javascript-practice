ed25108ce02bddb955759533fca40b4d040829fb
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-15 17:22:29 +0000

    reformat

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -4,29 +4,29 @@ import "./App.css";
 import { ChatContainer } from "./components/ChatContainer.jsx";
 
 function App() {
-	const [test, setTest] = useState([]);
+    const [test, setTest] = useState([]);
 
-	const getTest = async () =>
-		await fetch("http://localhost:5000/")
-			.then((res) => res.json())
-			.then((t) => setTest(t))
-			.catch((err) => console.log(err));
+    const getTest = async () =>
+        await fetch("http://localhost:5000/")
+            .then((res) => res.json())
+            .then((t) => setTest(t))
+            .catch((err) => console.log(err));
 
-	useEffect(() => getTest(), []);
+    useEffect(() => getTest(), []);
 
-	return (
-		<div>
-			<h1>Chat App React</h1>
-			<ChatContainer />
-			<div>
-				{test.map((el) => (
-					<p key={el.id}>
-						{el.id}: {el.nickname}
-					</p>
-				))}
-			</div>
-		</div>
-	);
+    return (
+        <div>
+            <h1>Chat App React</h1>
+            <ChatContainer />
+            <div>
+                {test.map((el) => (
+                    <p key={el.id}>
+                        {el.id}: {el.nickname}
+                    </p>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
