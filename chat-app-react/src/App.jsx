1503e5de92b44974726cda9fb29a2552a86e5a66
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-16 15:27:56 +0000

    cleanup

--- chat-app-react/src/App.jsx
+++ chat-app-react/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./sanitize.css";
 import "./App.css";
 import { ChatContainer } from "./components/ChatContainer.jsx";
