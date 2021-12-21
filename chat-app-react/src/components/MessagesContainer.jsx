1989e86954220680f6c386701b27bf8cbc4873f9
Rado <43698977+BaiRado@users.noreply.github.com>
2021-12-21 13:38:13 +0000

    reformatted

--- chat-app-react/src/components/MessagesContainer.jsx
+++ chat-app-react/src/components/MessagesContainer.jsx
@@ -1,9 +1,9 @@
 import { Message } from "./Message";
 
 export const MessagesContainer = (props) => (
-	<div className="messagesContainer">
-		{props.messages.map((text, idx) => (
-			<Message key={idx} text={text} />
-		))}
-	</div>
+    <div className="messagesContainer">
+        {props.messages.map((text, idx) => (
+            <Message key={idx} text={text} />
+        ))}
+    </div>
 );
